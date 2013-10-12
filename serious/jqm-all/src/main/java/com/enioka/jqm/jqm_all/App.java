ede44dfd24413834bbbc62779d9f8024301887a1
marcanpilami <marsu_pilami@msn.com>
2013-10-12 12:45:09 +0200

    Added "all" project to ease testing & compiling
    Signed-off-by: marcanpilami <marsu_pilami@msn.com>

---
+++ serious/jqm-all/src/main/java/com/enioka/jqm/jqm_all/App.java
@@ -0,0 +1,13 @@
+package com.enioka.jqm.jqm_all;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
