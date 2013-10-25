586323bfbec7ec77a5491e8a10a62ee9560aefe1
pco <pierre.coppee@enioka.com>
2013-10-25 15:45:21 +0200

    tests renamed

---
+++ serious/JobBaseAPI/src/test/java/com/enioka/jqm/tests/ParameterTest.java
@@ -0,0 +1,133 @@
+package com.enioka.jqm.tests;
+
+import java.util.ArrayList;
+
+import javax.persistence.EntityManager;
+
+import org.hsqldb.Server;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.enioka.jqm.api.Dispatcher;
+import com.enioka.jqm.api.JobDefinition;
+import com.enioka.jqm.jpamodel.JobDef;
+import com.enioka.jqm.jpamodel.JobDefParameter;
+import com.enioka.jqm.tools.CreationTools;
+import com.enioka.jqm.tools.JqmEngine;
+
+public class ParameterTest
+{
+	public static Server s;
+
+	@BeforeClass
+	public static void testInit()
+	{
+		s = new Server();
+		s.setDatabaseName(0, "testdbengine");
+		s.setDatabasePath(0, "mem:testdbengine");
+		s.setLogWriter(null);
+		s.setSilent(true);
+		s.start();
+
+		Dispatcher.resetEM();
+		com.enioka.jqm.tools.Helpers.resetEmf();
+	}
+
+	@AfterClass
+	public static void stop()
+	{
+		s.shutdown();
+	}
+
+	@Test
+	public void testMixParameters() throws Exception
+	{
+		EntityManager em = com.enioka.jqm.tools.Helpers.getNewEm();
+		Helpers.cleanup(em);
+		Helpers.createLocalNode(em);
+
+		ArrayList<JobDefParameter> jdargs = new ArrayList<JobDefParameter>();
+		JobDefParameter jdp = CreationTools.createJobDefParameter("arg1", "argument1", em);
+		JobDefParameter jdp2 = CreationTools.createJobDefParameter("arg2", "Franquin", em);
+		jdargs.add(jdp);
+		jdargs.add(jdp2);
+
+		JobDef jdDemoMaven = CreationTools.createJobDef(true, "App", jdargs, "./testprojects/CheckArgs/",
+				"./testprojects/CheckArgs/CheckArgs.jar", Helpers.qVip, 42, "MarsuApplication", 42, "Franquin", "ModuleMachin", "other",
+				"other", "other", false, em);
+
+		JobDefinition j = new JobDefinition("MarsuApplication", "MAG");
+		j.addParameter("arg2", "argument2");
+
+		Dispatcher.enQueue(j);
+
+		JqmEngine engine1 = new JqmEngine();
+		engine1.start(new String[] { "localhost" });
+		Thread.sleep(5000);
+		engine1.stop();
+
+		Assert.assertEquals(true, true);
+	}
+
+	@Test
+	public void testDefaultParameters() throws Exception
+	{
+		EntityManager em = com.enioka.jqm.tools.Helpers.getNewEm();
+		Helpers.cleanup(em);
+		Helpers.createLocalNode(em);
+
+		ArrayList<JobDefParameter> jdargs = new ArrayList<JobDefParameter>();
+		JobDefParameter jdp = CreationTools.createJobDefParameter("arg1", "argument1", em);
+		JobDefParameter jdp2 = CreationTools.createJobDefParameter("arg2", "argument2", em);
+		jdargs.add(jdp);
+		jdargs.add(jdp2);
+
+		JobDef jdDemoMaven = CreationTools.createJobDef(true, "App", jdargs, "./testprojects/CheckArgs/",
+				"./testprojects/CheckArgs/CheckArgs.jar", Helpers.qVip, 42, "MarsuApplication", 42, "Franquin", "ModuleMachin", "other",
+				"other", "other", false, em);
+
+		JobDefinition j = new JobDefinition("MarsuApplication", "MAG");
+
+		Dispatcher.enQueue(j);
+
+		JqmEngine engine1 = new JqmEngine();
+		engine1.start(new String[] { "localhost" });
+		Thread.sleep(5000);
+		engine1.stop();
+
+		Assert.assertEquals(true, true);
+	}
+
+	@Test
+	public void testOverrideParmeters() throws Exception
+	{
+		EntityManager em = com.enioka.jqm.tools.Helpers.getNewEm();
+		Helpers.cleanup(em);
+		Helpers.createLocalNode(em);
+
+		ArrayList<JobDefParameter> jdargs = new ArrayList<JobDefParameter>();
+		JobDefParameter jdp = CreationTools.createJobDefParameter("arg1", "Gaston Lagaffe", em);
+		JobDefParameter jdp2 = CreationTools.createJobDefParameter("arg2", "Franquin", em);
+		jdargs.add(jdp);
+		jdargs.add(jdp2);
+
+		JobDef jdDemoMaven = CreationTools.createJobDef(true, "App", jdargs, "./testprojects/CheckArgs/",
+				"./testprojects/CheckArgs/CheckArgs.jar", Helpers.qVip, 42, "MarsuApplication", 42, "Franquin", "ModuleMachin", "other",
+				"other", "other", false, em);
+
+		JobDefinition j = new JobDefinition("MarsuApplication", "MAG");
+		j.addParameter("arg1", "argument1");
+		j.addParameter("arg2", "argument2");
+
+		Dispatcher.enQueue(j);
+
+		JqmEngine engine1 = new JqmEngine();
+		engine1.start(new String[] { "localhost" });
+		Thread.sleep(5000);
+		engine1.stop();
+
+		Assert.assertEquals(true, true);
+	}
+}
---
+++ serious/JobBaseAPI/src/test/java/com/enioka/jqm/tests/QueueTest.java
@@ -0,0 +1,176 @@
+package com.enioka.jqm.tests;
+
+import java.util.ArrayList;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import org.hsqldb.Server;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.enioka.jqm.api.Dispatcher;
+import com.enioka.jqm.api.JobDefinition;
+import com.enioka.jqm.jpamodel.JobDef;
+import com.enioka.jqm.jpamodel.JobDefParameter;
+import com.enioka.jqm.jpamodel.JobInstance;
+import com.enioka.jqm.tools.CreationTools;
+import com.enioka.jqm.tools.JqmEngine;
+
+public class QueueTest
+{
+
+	public static Server s;
+
+	@BeforeClass
+	public static void testInit()
+	{
+		s = new Server();
+		s.setDatabaseName(0, "testdbengine");
+		s.setDatabasePath(0, "mem:testdbengine");
+		s.setLogWriter(null);
+		s.setSilent(true);
+		s.start();
+
+		Dispatcher.resetEM();
+		com.enioka.jqm.tools.Helpers.resetEmf();
+	}
+
+	@AfterClass
+	public static void stop()
+	{
+		s.shutdown();
+	}
+
+	@Test
+	public void testMaxThreadNormal() throws Exception
+	{
+		EntityManager em = com.enioka.jqm.tools.Helpers.getNewEm();
+		Helpers.cleanup(em);
+		Helpers.createLocalNode(em);
+		ArrayList<JobInstance> job = null;
+
+		ArrayList<JobDefParameter> jdargs = new ArrayList<JobDefParameter>();
+		JobDefParameter jdp = CreationTools.createJobDefParameter("arg", "POUPETTE", em);
+		jdargs.add(jdp);
+
+		JobDef jdDemoMaven = CreationTools.createJobDef(true, "App", jdargs, "./testprojects/jqm-test-datetimemaven/",
+				"./testprojects/jqm-test-datetimemaven/jqm-test-datetimemaven.jar", Helpers.qNormal, 42, "MarsuApplication", 42,
+				"Franquin", "ModuleMachin", "other", "other", "other", false, em);
+
+		JobDefinition j = new JobDefinition("MarsuApplication", "MAG");
+
+		Dispatcher.enQueue(j);
+		Dispatcher.enQueue(j);
+		Dispatcher.enQueue(j);
+
+		int i = 0;
+
+		JqmEngine engine1 = new JqmEngine();
+		engine1.start(new String[] { "localhost" });
+
+		while (i < 5)
+		{
+			EntityManager emm = com.enioka.jqm.tools.Helpers.getNewEm();
+			Dispatcher.enQueue(j);
+			Dispatcher.enQueue(j);
+			Dispatcher.enQueue(j);
+			Dispatcher.enQueue(j);
+			Dispatcher.enQueue(j);
+			Dispatcher.enQueue(j);
+			Dispatcher.enQueue(j);
+			Dispatcher.enQueue(j);
+			Dispatcher.enQueue(j);
+			Dispatcher.enQueue(j);
+			Dispatcher.enQueue(j);
+			Dispatcher.enQueue(j);
+			Dispatcher.enQueue(j);
+			Dispatcher.enQueue(j);
+			Dispatcher.enQueue(j);
+			Thread.sleep(3000);
+			Helpers.printJobInstanceTable();
+			TypedQuery<JobInstance> query = emm
+					.createQuery("SELECT j FROM JobInstance j WHERE j.state IS NOT :s AND j.state IS NOT :ss ORDER BY j.position ASC",
+							JobInstance.class);
+			query.setParameter("s", "SUBMITTED").setParameter("ss", "ENDED");
+			job = (ArrayList<JobInstance>) query.getResultList();
+
+			if (job.size() > 2)
+				Assert.assertEquals(false, true);
+			i++;
+		}
+		engine1.stop();
+
+		Assert.assertEquals(true, true);
+	}
+
+	@Test
+	public void testMaxThreadVip() throws Exception
+	{
+		EntityManager em = com.enioka.jqm.tools.Helpers.getNewEm();
+		Helpers.cleanup(em);
+		Helpers.createLocalNode(em);
+		ArrayList<JobInstance> job = null;
+
+		ArrayList<JobDefParameter> jdargs = new ArrayList<JobDefParameter>();
+		JobDefParameter jdp = CreationTools.createJobDefParameter("arg", "POUPETTE", em);
+		jdargs.add(jdp);
+
+		JobDef jdDemoMaven = CreationTools.createJobDef(true, "App", jdargs, "./testprojects/jqm-test-datetimemaven/",
+				"./testprojects/jqm-test-datetimemaven/jqm-test-datetimemaven.jar", Helpers.qVip, 42, "MarsuApplication", 42, "Franquin",
+				"ModuleMachin", "other", "other", "other", false, em);
+
+		JobDefinition j = new JobDefinition("MarsuApplication", "MAG");
+
+		Dispatcher.enQueue(j);
+		Dispatcher.enQueue(j);
+		Dispatcher.enQueue(j);
+		Dispatcher.enQueue(j);
+		Dispatcher.enQueue(j);
+		Dispatcher.enQueue(j);
+		Dispatcher.enQueue(j);
+		Dispatcher.enQueue(j);
+		Dispatcher.enQueue(j);
+
+		int i = 0;
+
+		JqmEngine engine1 = new JqmEngine();
+		engine1.start(new String[] { "localhost" });
+
+		while (i < 5)
+		{
+			EntityManager emm = com.enioka.jqm.tools.Helpers.getNewEm();
+			Dispatcher.enQueue(j);
+			Dispatcher.enQueue(j);
+			Dispatcher.enQueue(j);
+			Dispatcher.enQueue(j);
+			Dispatcher.enQueue(j);
+			Dispatcher.enQueue(j);
+			Dispatcher.enQueue(j);
+			Dispatcher.enQueue(j);
+			Dispatcher.enQueue(j);
+			Dispatcher.enQueue(j);
+			Dispatcher.enQueue(j);
+			Dispatcher.enQueue(j);
+			Dispatcher.enQueue(j);
+			Dispatcher.enQueue(j);
+			Dispatcher.enQueue(j);
+			Thread.sleep(3000);
+			Helpers.printJobInstanceTable();
+			TypedQuery<JobInstance> query = emm
+					.createQuery("SELECT j FROM JobInstance j WHERE j.state IS NOT :s AND j.state IS NOT :ss ORDER BY j.position ASC",
+							JobInstance.class);
+			query.setParameter("s", "SUBMITTED").setParameter("ss", "ENDED");
+			job = (ArrayList<JobInstance>) query.getResultList();
+
+			if (job.size() > 3)
+				Assert.assertEquals(false, true);
+			i++;
+		}
+		engine1.stop();
+
+		Assert.assertEquals(true, true);
+	}
+}
