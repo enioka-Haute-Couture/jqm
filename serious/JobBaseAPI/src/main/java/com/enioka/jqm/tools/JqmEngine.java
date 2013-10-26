d238a98e7ba1a1edefeaff91abdd06301901a26b
marcanpilami <marsu_pilami@msn.com>
2013-10-26 23:39:45 +0200

    Packaging: an angine will now create some configuration at (first) startup
    Signed-off-by: marcanpilami <marsu_pilami@msn.com>

--- serious/JobBaseAPI/src/main/java/com/enioka/jqm/tools/JqmEngine.java
+++ serious/JobBaseAPI/src/main/java/com/enioka/jqm/tools/JqmEngine.java
@@ -24,6 +24,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.NonUniqueResultException;
 
 import org.apache.log4j.Logger;
 import org.eclipse.jetty.server.Server;
@@ -32,8 +34,10 @@ import org.eclipse.jetty.servlet.ServletHolder;
 
 import com.enioka.jqm.jndi.JndiContext;
 import com.enioka.jqm.jndi.JndiContextFactory;
+import com.enioka.jqm.jpamodel.DatabaseProp;
 import com.enioka.jqm.jpamodel.DeploymentParameter;
 import com.enioka.jqm.jpamodel.Node;
+import com.enioka.jqm.jpamodel.Queue;
 
 class JqmEngine
 {
@@ -44,7 +48,7 @@ class JqmEngine
 	private Map<String, URL[]> cache = new HashMap<String, URL[]>();
 	private Server server = null;
 	private JndiContext jndiCtx = null;
-	private static Logger jqmlogger = Logger.getLogger(JarClassLoader.class);
+	private static Logger jqmlogger = Logger.getLogger(JqmEngine.class);
 
 	/**
 	 * Starts the engine
@@ -57,8 +61,8 @@ class JqmEngine
 	{
 		java.lang.System.setProperty("log4j.debug", "true");
 
-		node = em.createQuery("SELECT n FROM Node n WHERE n.listeningInterface = :l", Node.class).setParameter("l", args[0])
-				.getSingleResult();
+		// Node configuration is in the database
+		node = checkAndUpdateNode(args[0]);
 
 		// JNDI
 		if (jndiCtx == null)
@@ -114,5 +118,108 @@ class JqmEngine
 		{
 
 		}
+	}
+
+	private Node checkAndUpdateNode(String nodeName)
+	{
+		em.getTransaction().begin();
+
+		// Node
+		Node n = null;
+		try
+		{
+			n = em.createQuery("SELECT n FROM Node n WHERE n.listeningInterface = :l", Node.class).setParameter("l", nodeName)
+					.getSingleResult();
+			jqmlogger.info("Node " + nodeName + " was found in the configuration");
+		} catch (NoResultException e)
+		{
+			jqmlogger.info("Node " + nodeName + " does not exist in the configuration and will be created with default values");
+			n = new Node();
+			n.setDlRepo(System.getProperty("user.dir") + "/outputfiles");
+			n.setListeningInterface(nodeName);
+			n.setPort(1789);
+			em.persist(n);
+		}
+
+		// Default queue
+		Queue q = null;
+		long i = (Long) em.createQuery("SELECT COUNT(qu) FROM Queue qu").getSingleResult();
+		jqmlogger.info("There are " + i + " queues defined in the database");
+		if (i == 0L)
+		{
+			q = new Queue();
+			q.setDefaultQueue(true);
+			q.setDescription("default queue");
+			q.setMaxTempInQueue(1024);
+			q.setMaxTempRunning(1024);
+			q.setName("DEFAULT");
+			em.persist(q);
+
+			jqmlogger.info("A default queue was created in the configuration");
+		}
+		else
+		{
+			try
+			{
+				q = em.createQuery("SELECT q FROM Queue q WHERE q.defaultQueue = true", Queue.class).getSingleResult();
+				jqmlogger.info("Default queue is named " + q.getName());
+			} catch (NonUniqueResultException e)
+			{
+				// Faulty configuration, but why not
+				q = em.createQuery("SELECT q FROM Queue q", Queue.class).getResultList().get(0);
+				q.setDefaultQueue(true);
+				jqmlogger.warn("Queue " + q.getName() + " was modified to become the default queue as there was no default queue");
+			} catch (NoResultException e)
+			{
+				// Faulty configuration, but why not
+				q = em.createQuery("SELECT q FROM Queue q", Queue.class).getResultList().get(0);
+				q.setDefaultQueue(true);
+				jqmlogger.warn("Queue " + q.getName() + " was modified to become the default queue as there was no default queue");
+			}
+		}
+
+		// Deployment parameter
+		DeploymentParameter dp = null;
+		i = (Long) em.createQuery("SELECT COUNT(dp) FROM DeploymentParameter dp WHERE dp.node = :localnode").setParameter("localnode", n)
+				.getSingleResult();
+		if (i == 0)
+		{
+			dp = new DeploymentParameter();
+			dp.setClassId(1);
+			dp.setNbThread(5);
+			dp.setNode(n);
+			dp.setPollingInterval(1000);
+			dp.setQueue(q);
+			em.persist(dp);
+			jqmlogger.info("This node will poll from the default queue with default parameters");
+		}
+		else
+		{
+			jqmlogger.info("This node is already configured to take jobs from the default queue");
+		}
+
+		// JNDI alias for the JDBC connection to the JQM database
+		DatabaseProp localDb = null;
+		try
+		{
+			localDb = (DatabaseProp) em.createQuery("SELECT dp FROM DatabaseProp dp WHERE dp.name = 'jdbc/jqm'").getSingleResult();
+			jqmlogger.info("The jdbc/jqm alias already exists and references " + localDb.getUrl());
+		} catch (NoResultException e)
+		{
+			Map<String, Object> props = em.getEntityManagerFactory().getProperties();
+			localDb = new DatabaseProp();
+			localDb.setDriver((String) props.get("javax.persistence.jdbc.driver"));
+			localDb.setName("jdbc/jqm");
+			localDb.setPwd((String) props.get("javax.persistence.jdbc.password"));
+			localDb.setUrl((String) props.get("javax.persistence.jdbc.url"));
+			localDb.setUserName((String) props.get("javax.persistence.jdbc.user"));
+			em.persist(localDb);
+
+			jqmlogger.info("A  JNDI alias towards the JQM db has been created. It references: " + localDb.getUrl());
+		}
+
+		// Done
+		em.getTransaction().commit();
+		return n;
 	}
 }
