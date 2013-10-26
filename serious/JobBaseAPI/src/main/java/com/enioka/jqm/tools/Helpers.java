762890148f2c6fa51dff5235d409b9810a3dec14
marcanpilami <marsu_pilami@msn.com>
2013-10-26 23:41:10 +0200

    Packaging: engine will read conf/db.properties for db connection parameters and only use the persistence.xml values if that file does not exist
    Signed-off-by: marcanpilami <marsu_pilami@msn.com>

--- serious/JobBaseAPI/src/main/java/com/enioka/jqm/tools/Helpers.java
+++ serious/JobBaseAPI/src/main/java/com/enioka/jqm/tools/Helpers.java
@@ -1,8 +1,15 @@
 package com.enioka.jqm.tools;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+
+import org.apache.log4j.Logger;
 
 import com.enioka.jqm.jpamodel.Deliverable;
 import com.enioka.jqm.jpamodel.History;
@@ -10,11 +17,15 @@ import com.enioka.jqm.jpamodel.Message;
 
 public final class Helpers
 {
+	private static final String PERSISTENCE_UNIT = "jobqueue-api-pu";
+	private static Logger jqmlogger = Logger.getLogger(Helpers.class);
+
 	// The one and only EMF in the engine.
-	private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("jobqueue-api-pu");
+	private static EntityManagerFactory emf = createFactory();
 
 	/**
 	 * Get a fresh EM on the jobqueue-api-pu persistence Unit
+	 * 
 	 * @return an EntityManager
 	 */
 	public static EntityManager getNewEm()
@@ -22,8 +33,29 @@ public final class Helpers
 		return emf.createEntityManager();
 	}
 
+	private static EntityManagerFactory createFactory()
+	{
+		Properties p = new Properties();
+		try
+		{
+			p.load(new FileInputStream("conf/db.properties"));
+			return Persistence.createEntityManagerFactory(PERSISTENCE_UNIT, p);
+		} catch (FileNotFoundException e)
+		{
+			// No properties file means we use the test HSQLDB (file, not in-memory) as specified in the persistence.xml
+			return Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
+		} catch (IOException e)
+		{
+			jqmlogger.fatal("conf/db.properties file is invalid", e);
+			System.exit(1);
+			// Stupid, just for Eclipse's parser and therefore avoid red lines...
+			return null;
+		}
+	}
+
 	/**
-	 * For internal test use only
+	 * For internal test use only <br/>
+	 * <bold>WARNING</bold> This will invalidate all open EntityManagers!
 	 */
 	static void resetEmf()
 	{
@@ -31,12 +63,12 @@ public final class Helpers
 		{
 			emf.close();
 		}
-		emf = Persistence.createEntityManagerFactory("jobqueue-api-pu");
+		emf = createFactory();
 	}
 
 	/**
-	 * Create a text message that will be stored in the database.
-	 * Must be called inside a JPA transaction.
+	 * Create a text message that will be stored in the database. Must be called inside a JPA transaction.
+	 * 
 	 * @param textMessage
 	 * @param history
 	 * @param em
@@ -53,16 +85,21 @@ public final class Helpers
 		return m;
 	}
 
-
 	/**
-	 * Create a Deliverable inside the datbase that will track a file created by a JobInstance
-	 * Must be called from inside a JPA transaction
-	 * @param fp FilePath (relative to a root directory - cf. Node)
-	 * @param fn FileName
-	 * @param hp HashPath
-	 * @param ff File family (may be null). E.g.: "daily report"
-	 * @param jobId Job Instance ID
-	 * @param em the EM to use.
+	 * Create a Deliverable inside the datbase that will track a file created by a JobInstance Must be called from inside a JPA transaction
+	 * 
+	 * @param fp
+	 *            FilePath (relative to a root directory - cf. Node)
+	 * @param fn
+	 *            FileName
+	 * @param hp
+	 *            HashPath
+	 * @param ff
+	 *            File family (may be null). E.g.: "daily report"
+	 * @param jobId
+	 *            Job Instance ID
+	 * @param em
+	 *            the EM to use.
 	 * @return
 	 */
 	public static Deliverable createDeliverable(String fp, String fn, String hp, String ff, Integer jobId, EntityManager em)
