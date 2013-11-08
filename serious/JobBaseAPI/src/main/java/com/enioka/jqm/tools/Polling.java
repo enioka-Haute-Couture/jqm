2cc7eb5b1df5203f588a680aac2d5be8515eddb8
marcanpilami <marsu_pilami@msn.com>
2013-11-08 15:45:28 +0100

    Sonar: removed dead code
    Signed-off-by: marcanpilami <marsu_pilami@msn.com>

--- serious/JobBaseAPI/src/main/java/com/enioka/jqm/tools/Polling.java
+++ serious/JobBaseAPI/src/main/java/com/enioka/jqm/tools/Polling.java
@@ -23,7 +23,6 @@ import java.util.ArrayList;
 import java.util.Map;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
 import javax.persistence.TypedQuery;
 
 import org.apache.log4j.Logger;
@@ -64,7 +63,7 @@ class Polling implements Runnable
 	{
 		// Get the list of all jobInstance with the queue definded ordered by position
 		TypedQuery<JobInstance> query = em.createQuery(
-				"SELECT j FROM JobInstance j WHERE j.queue.name = :q AND j.state = :s ORDER BY j.position ASC", JobInstance.class);
+		        "SELECT j FROM JobInstance j WHERE j.queue.name = :q AND j.state = :s ORDER BY j.position ASC", JobInstance.class);
 		query.setParameter("q", queue.getName()).setParameter("s", "SUBMITTED");
 		job = (ArrayList<JobInstance>) query.getResultList();
 
@@ -83,29 +82,11 @@ class Polling implements Runnable
 		return job;
 	}
 
-	void executionStatus()
-	{
-		EntityManager em = Helpers.getNewEm();
-		EntityTransaction transac = em.getTransaction();
-		transac.begin();
-
-		History h = em.createQuery("SELECT h FROM History h WHERE h.id = :j", History.class).setParameter("j", job.get(0).getId())
-				.getSingleResult();
-
-		Helpers.createMessage("Status updated: RUNNING", h, em);
-
-		em.createQuery("UPDATE JobInstance j SET j.state = :msg WHERE j.id = :j)").setParameter("j", job.get(0).getId())
-				.setParameter("msg", "RUNNING").executeUpdate();
-
-		transac.commit();
-		em.close();
-	}
-
 	protected void HighlanderPollingMode(JobInstance currentJob, EntityManager em)
 	{
 		ArrayList<JobInstance> jobs = (ArrayList<JobInstance>) em
-				.createQuery("SELECT j FROM JobInstance j WHERE j.id IS NOT :refid AND j.jd.applicationName = :n", JobInstance.class)
-				.setParameter("refid", currentJob.getId()).setParameter("n", currentJob.getJd().getApplicationName()).getResultList();
+		        .createQuery("SELECT j FROM JobInstance j WHERE j.id IS NOT :refid AND j.jd.applicationName = :n", JobInstance.class)
+		        .setParameter("refid", currentJob.getId()).setParameter("n", currentJob.getJd().getApplicationName()).getResultList();
 
 		for (JobInstance j : jobs)
 		{
@@ -153,7 +134,7 @@ class Polling implements Runnable
 				em.getTransaction().begin();
 
 				JobInstance tt = em.createQuery("SELECT j FROM JobInstance j WHERE j.id = :j", JobInstance.class)
-						.setParameter("j", ji.getId()).getSingleResult();
+				        .setParameter("j", ji.getId()).getSingleResult();
 
 				tt.setNode(dp.getNode());
 
@@ -161,10 +142,10 @@ class Polling implements Runnable
 				// .setParameter("n", dp.getNode()).executeUpdate();
 
 				em.createQuery("UPDATE History h SET h.node = :n WHERE h.jobInstance.id = :j").setParameter("j", ji.getId())
-						.setParameter("n", dp.getNode()).executeUpdate();
+				        .setParameter("n", dp.getNode()).executeUpdate();
 
 				History h = em.createQuery("SELECT h FROM History h WHERE h.jobInstance = :j", History.class).setParameter("j", ji)
-						.getSingleResult();
+				        .getSingleResult();
 
 				Message m = new Message();
 
@@ -172,20 +153,20 @@ class Polling implements Runnable
 				m.setHistory(h);
 
 				em.createQuery("UPDATE JobInstance j SET j.state = :msg WHERE j.id = :j").setParameter("j", ji.getId())
-						.setParameter("msg", "ATTRIBUTED").executeUpdate();
+				        .setParameter("msg", "ATTRIBUTED").executeUpdate();
 
 				em.persist(m);
 				em.getTransaction().commit();
 
 				JobInstance t = em.createQuery("SELECT j FROM JobInstance j WHERE j.id = :j", JobInstance.class)
-						.setParameter("j", ji.getId()).getSingleResult();
+				        .setParameter("j", ji.getId()).getSingleResult();
 
 				History th = em.createQuery("SELECT j FROM History j WHERE j.jobInstance.id = :j", History.class)
-						.setParameter("j", ji.getId()).getSingleResult();
+				        .setParameter("j", ji.getId()).getSingleResult();
 
 				jqmlogger.debug("The job " + t.getId() + " have been updated with the node: " + t.getNode().getListeningInterface());
 				jqmlogger.debug("The job history " + th.getId() + " have been updated with the node: "
-						+ th.getNode().getListeningInterface());
+				        + th.getNode().getListeningInterface());
 
 				actualNbThread++;
 
