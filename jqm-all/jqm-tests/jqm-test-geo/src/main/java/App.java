d62f02f20f00b1106186e2413c6a2ddc0aad6df9
marcanpilami <marsu_pilami@msn.com>
2013-12-03 16:12:36 +0100

    Geo test now has an end.
    Signed-off-by: marcanpilami <marsu_pilami@msn.com>

--- jqm-all/jqm-tests/jqm-test-geo/src/main/java/App.java
+++ jqm-all/jqm-tests/jqm-test-geo/src/main/java/App.java
@@ -3,16 +3,21 @@ import java.util.Map;
 
 import com.enioka.jqm.api.JobBase;
 
-
-
-public class App extends JobBase{
+public class App extends JobBase
+{
 
 	@Override
 	public void start()
 	{
 		System.out.println("BEGINING GEOJOB: " + this.parameters.get("nbJob"));
 		Map<String, String> p = new HashMap<String, String>();
-		p.put("nbJob", (Integer.parseInt(this.parameters.get("nbJob") + 1) + ""));
+		p.put("nbJob", ((Integer.parseInt(this.parameters.get("nbJob")) + 1) + ""));
+
+		if (Integer.parseInt(this.parameters.get("nbJob")) >= 5)
+		{
+			System.out.println("END OF GEO - reached 5th generation");
+			return;
+		}
 
 		sendMsg("launching first job");
 		System.out.println("LAUNCHING FIRST JOB");
