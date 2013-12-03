f32003a1df2ac2a09bd78575e73b6d101233ed1e
marcanpilami <marsu_pilami@msn.com>
2013-12-03 16:12:15 +0100

    update package script
    Signed-off-by: marcanpilami <marsu_pilami@msn.com>

--- jqm-all/package.ps1
+++ jqm-all/package.ps1
@@ -1,16 +1,18 @@
 $ErrorActionPreference = "Stop"
 
 $destDir = "C:\JQM"
-$from = ".\JobBaseAPI"
+$from = ".\jqm-engine"
 
-cd jqm-all
-mvn  clean install -DskipTests
-mvn dependency:copy-dependencies
-cd ..
+mvn  clean install dependency:copy-dependencies -DskipTests
+if (-not $?)
+{
+    return
+}
 
 mkdir -Force $destDir
-mkdir -Force $destDir/conf,$destDir/jobs,$destDir/outputfiles,$destDir\jobs\jqm-test-fibo,$destDir\lib
+mkdir -Force $destDir/conf,$destDir/jobs,$destDir/outputfiles,$destDir\jobs\jqm-test-fibo,$destDir\jobs\jqm-test-geo,$destDir\lib
 cp -Force $from\target\jqm-engine*.jar $destDir
 cp -Force $from\target\dependency\* $destDir\lib
 cp -Force $from\lib\res.xsd $destDir\lib
 cp -force $from\testprojects\jqm-test-fibo\* $destDir\jobs\jqm-test-fibo
+cp -force $from\testprojects\jqm-test-geo\* $destDir\jobs\jqm-test-geo
