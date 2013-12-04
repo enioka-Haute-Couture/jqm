52fbc02c730a60cdd65a4ec1f7bc6858640d60fd
marcanpilami <marsu_pilami@msn.com>
2013-12-04 09:47:50 +0100

    Renamed jar
    Signed-off-by: marcanpilami <marsu_pilami@msn.com>

--- jqm-all/package.ps1
+++ jqm-all/package.ps1
@@ -11,7 +11,7 @@ if (-not $?)
 
 mkdir -Force $destDir
 mkdir -Force $destDir/conf,$destDir/jobs,$destDir/outputfiles,$destDir\jobs\jqm-test-fibo,$destDir\jobs\jqm-test-geo,$destDir\lib
-cp -Force $from\target\jqm-engine*.jar $destDir
+cp -Force $from\target\jqm-engine*.jar $destDir\jqm.jar
 cp -Force $from\target\dependency\* $destDir\lib
 cp -Force $from\lib\res.xsd $destDir\lib
 cp -force $from\testprojects\jqm-test-fibo\* $destDir\jobs\jqm-test-fibo
