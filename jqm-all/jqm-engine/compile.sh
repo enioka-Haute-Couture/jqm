c44de60754d2c87231b6ad73007ceee5263a202a
pco <pierre.coppee@enioka.com>
2013-12-31 12:07:10 +0100

    compile.sh deleted

--- jqm-all/jqm-engine/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-#mvn3 clean install -DskipTests dependency:copy-dependencies && cp target/jqm-engine-1.1.4-SNAPSHOT.jar ~/jqm/ && cp target/dependency/* ~/jqm/lib/
-
-mvn3 clean install -DskipTests && cp target/jqm-engine-1.1.4-SNAPSHOT.jar ~/jqm/
