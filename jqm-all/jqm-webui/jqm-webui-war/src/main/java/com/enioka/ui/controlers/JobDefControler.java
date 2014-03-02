27f111a9c1d1b3e0e323eb47e333d50f13200821
marcanpilami <marsu_pilami@msn.com>
2014-03-02 18:39:40 +0100

    WebUi migration step 1: make it work once again

---
+++ jqm-all/jqm-webui/jqm-webui-war/src/main/java/com/enioka/ui/controlers/JobDefControler.java
@@ -0,0 +1,115 @@
+package com.enioka.ui.controlers;
+
+import java.io.Serializable;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.model.ListDataModel;
+import javax.persistence.EntityManager;
+
+import org.primefaces.event.SelectEvent;
+import org.primefaces.event.UnselectEvent;
+import org.primefaces.model.SelectableDataModel;
+
+import com.enioka.jqm.api.JobRequest;
+import com.enioka.jqm.api.JqmClientFactory;
+import com.enioka.jqm.jpamodel.JobDef;
+import com.enioka.jqm.test.helpers.CreationTools;
+import com.enioka.jqm.test.helpers.TestHelpers;
+import com.enioka.ui.helpers.Db;
+
+@ManagedBean(eager = true)
+@SessionScoped
+public class JobDefControler extends ListDataModel<JobDef> implements Serializable, SelectableDataModel<JobDef>
+{
+    private static final long serialVersionUID = -608970776489109835L;
+
+    EntityManager em = null;
+
+    private JobDef selected = null;
+    private String userName = "user";
+
+    public JobDefControler()
+    {
+        em = Db.getEm();
+
+        if (em.createQuery("SELECT COUNT(n) FROM Node n", Long.class).getSingleResult().equals(0l))
+        {
+            TestHelpers.createLocalNode(em);
+            CreationTools.createJobDef(null, true, "com.enioka.jqm.tests.App", null, null, "jqm-tests/jqm-test-fibo/target/test.jar",
+                    TestHelpers.qVip, 42, "Fibo", null, "Franquin", "ModuleMachin", "other1", "other2", false, em);
+            CreationTools.createJobDef(null, true, "com.enioka.jqm.tests.App", null, null, "jqm-tests/jqm-test-fibo/target/test.jar",
+                    TestHelpers.qVip, 42, "Fibo2", null, "Franquin", "ModuleMachin", "other1", "other2", false, em);
+            CreationTools.createJobDef(null, true, "com.enioka.jqm.tests.App", null, null, "jqm-tests/jqm-test-fibo/target/test.jar",
+                    TestHelpers.qVip, 42, "Fibo3", null, "Franquin", "ModuleMachin", "other1", "other2", false, em);
+            CreationTools.createJobDef(null, true, "com.enioka.jqm.tests.App", null, null, "jqm-tests/jqm-test-fibo/target/test.jar",
+                    TestHelpers.qVip, 42, "Fibo4", null, "Franquin", "ModuleMachin", "other1", "other2", false, em);
+            CreationTools.createJobDef(null, true, "com.enioka.jqm.tests.App", null, null, "jqm-tests/jqm-test-fibo/target/test.jar",
+                    TestHelpers.qVip, 42, "Fibo5", null, "Franquin", "ModuleMachin", "other1", "other2", false, em);
+        }
+    }
+
+    public ListDataModel<JobDef> getJobs()
+    {
+        this.setWrappedData(em.createQuery("SELECT j FROM JobDef j", JobDef.class).getResultList());
+        return this;
+    }
+
+    public void enqueue()
+    {
+        JobRequest jr = new JobRequest(this.selected.getApplicationName(), this.userName);
+        int i = JqmClientFactory.getClient().enqueue(jr);
+
+        FacesMessage msg = new FacesMessage("Enqueue done", "request number " + i);
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+
+    public void setSelectedJob(JobDef job)
+    {
+        selected = job;
+    }
+
+    public JobDef getSelectedJob()
+    {
+        return selected;
+    }
+
+    public String getUserName()
+    {
+        return userName;
+    }
+
+    public void setUserName(String userName)
+    {
+        this.userName = userName;
+    }
+
+    public void onRowSelect(SelectEvent event)
+    {
+        FacesMessage msg = new FacesMessage("JD Selected", ((JobDef) event.getObject()).getApplicationName());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+
+    public void onRowUnselect(UnselectEvent event)
+    {
+        FacesMessage msg = new FacesMessage("JD Unselected", ((JobDef) event.getObject()).getApplicationName());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+
+    @Override
+    public Object getRowKey(JobDef object)
+    {
+        return object.getId();
+    }
+
+    @Override
+    public JobDef getRowData(String rowKey)
+    {
+        EntityManager em = Db.getEm();
+        JobDef jd = em.find(JobDef.class, Integer.parseInt(rowKey));
+        em.close();
+        return jd;
+    }
+}
---
+++ jqm-all/jqm-webui/jqm-webui-war/src/main/java/com/enioka/ui/controlers/JobInstanceControler.java
@@ -0,0 +1,68 @@
+package com.enioka.ui.controlers;
+
+import java.io.Serializable;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.model.ListDataModel;
+
+import org.primefaces.model.SelectableDataModel;
+
+import com.enioka.jqm.api.JobInstance;
+import com.enioka.jqm.api.JqmClientFactory;
+
+@ManagedBean(eager = true)
+@SessionScoped
+public class JobInstanceControler extends ListDataModel<JobInstance> implements Serializable, SelectableDataModel<JobInstance>
+{
+    private static final long serialVersionUID = 7869897762565932002L;
+    private JobInstance selected = null;
+
+    @PostConstruct
+    public void init()
+    {
+        FacesContext.getCurrentInstance().getExternalContext().getSession(true);
+        getJobs();
+    }
+
+    public void stop()
+    {
+        JqmClientFactory.getClient().cancelJob(selected.getId());
+    }
+
+    public ListDataModel<JobInstance> getJobs()
+    {
+        this.setWrappedData(JqmClientFactory.getClient().getActiveJobs());
+        return this;
+    }
+
+    public ListDataModel<JobInstance> getHistoryJobs()
+    {
+        this.setWrappedData(JqmClientFactory.getClient().getJobs());
+        return this;
+    }
+
+    public JobInstance getSelected()
+    {
+        return selected;
+    }
+
+    public void setSelected(JobInstance selected)
+    {
+        this.selected = selected;
+    }
+
+    @Override
+    public Object getRowKey(JobInstance object)
+    {
+        return object.getId();
+    }
+
+    @Override
+    public JobInstance getRowData(String rowKey)
+    {
+        return JqmClientFactory.getClient().getJob(Integer.parseInt(rowKey));
+    }
+}
