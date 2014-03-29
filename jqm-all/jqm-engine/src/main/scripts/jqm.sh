e016e1a130d42ccd8f8009f5e15b67c53582ba91
Sebastien Renard <Sebastien.Renard@enioka.com>
2014-03-29 17:46:49 +0100

    Move stdout/stderr to logs dir Fix #41

--- jqm-all/jqm-engine/src/main/scripts/jqm.sh
+++ jqm-all/jqm-engine/src/main/scripts/jqm.sh
@@ -16,7 +16,7 @@ then
 	JQM_NODE=$USER
 fi
 
-JAVA=${JAVA_HOME:-/usr/java7_64/jre/}/bin/java
+JAVA=${JAVA_HOME:-/usr/java6_64/jre/}/bin/java
 $JAVA -version  > /dev/null 2>&1
 if [[ $? -ne 0 ]]
 then
@@ -25,8 +25,8 @@ then
 fi
 
 
-JQM_PID_FILE=jqm_${JQM_NODE}.pid
-JQM_LOG_FILE=jqm_${JQM_NODE}.log
+JQM_PID_FILE=${LOCAL_DIR}/jqm_${JQM_NODE}.pid
+JQM_LOG_FILE=${LOCAL_DIR}/logs/jqm_${JQM_NODE}.log
 
 
 #############################################
@@ -35,61 +35,61 @@ JQM_LOG_FILE=jqm_${JQM_NODE}.log
 
 jqm_start() {
 	echo Starting JQM...
-	test -e ${LOCAL_DIR}/${JQM_PID_FILE}
+	test -e ${JQM_PID_FILE}
         if [[ $? -eq 0 ]]
         then
-                echo "PID file found (${LOCAL_DIR}/${JQM_PID_FILE})."
-		JQM_PID=$(cat ${LOCAL_DIR}/${JQM_PID_FILE})
+                echo "PID file found (${JQM_PID_FILE})."
+		JQM_PID=$(cat ${JQM_PID_FILE})
         	ps -p $JQM_PID > /dev/null 2>&1
         	if [[ $? -ne 0 ]]
         	then
                 	echo "PID file is here (${JQM_PID_FILE}) but JQM daemon is gone..."
                 	echo "Cleaning up pid file"
-                	rm ${LOCAL_DIR}/${JQM_PID_FILE}
+                	rm ${JQM_PID_FILE}
         	else
                		echo "JQM is already running with PID ${JQM_PID} for node ${JQM_NODE}"
                 	exit 1
         	fi
         fi
 	# We can go on...
-	nohup $JAVA -jar $JQM_JAR -startnode $JQM_NODE 2>&1 > ${LOCAL_DIR}/$JQM_LOG_FILE &
+	nohup $JAVA -jar $JQM_JAR -startnode $JQM_NODE 2>&1 > $JQM_LOG_FILE &
 	JQM_PID=$!
-	echo $JQM_PID > ${LOCAL_DIR}/${JQM_PID_FILE}
+	echo $JQM_PID > ${JQM_PID_FILE}
 	echo "JQM Started with pid ${JQM_PID}"
 }
 
 jqm_stop() {
 	echo Stopping JQM...
-	test -e ${LOCAL_DIR}/${JQM_PID_FILE}
+	test -e ${JQM_PID_FILE}
 	if [[ $? -ne 0 ]]
 	then
-        	echo "PID file not found (${LOCAL_DIR}/${JQM_PID_FILE})."
+        	echo "PID file not found (${JQM_PID_FILE})."
 		echo "Here are all jqm engine running on this host, choose the good one and kill it yourself:"
 		ps -ef |grep $JQM_JAR | grep -v grep
         	exit 1
 	fi
-	JQM_PID=$(cat ${LOCAL_DIR}/${JQM_PID_FILE})
+	JQM_PID=$(cat ${JQM_PID_FILE})
 	echo "Killing process $JQM_PID"
 	kill $JQM_PID
-	rm ${LOCAL_DIR}/${JQM_PID_FILE}
+	rm ${JQM_PID_FILE}
 }
 
 jqm_status() {
-	test -e ${LOCAL_DIR}/${JQM_PID_FILE}
+	test -e ${JQM_PID_FILE}
 	if [[ $? -ne 0 ]]
 	then
-        	echo "PID file not found (${LOCAL_DIR}/${JQM_PID_FILE})."
+        	echo "PID file not found (${JQM_PID_FILE})."
 		echo "Here are all jqm engine running on this host"
 		ps -ef |grep $JQM_JAR | grep -v grep
         	exit 1
 	fi
-	JQM_PID=$(cat ${LOCAL_DIR}/${JQM_PID_FILE})
+	JQM_PID=$(cat ${JQM_PID_FILE})
 	ps -p $JQM_PID > /dev/null 2>&1
 	if [[ $? -ne 0 ]]
 	then
 		echo "PID file is here (${JQM_PID_FILE}) but JQM daemon is gone..."
 		echo "Cleaning up pid file"
-		rm ${LOCAL_DIR}/${JQM_PID_FILE}
+		rm ${JQM_PID_FILE}
 		exit 1
 	else
 		echo "JQM is running with PID ${JQM_PID}"
