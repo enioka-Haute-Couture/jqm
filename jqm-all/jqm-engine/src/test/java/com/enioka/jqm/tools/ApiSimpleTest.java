1e9f15d83d0f2da038f3e63626820b5b36167936
marcanpilami <marsu_pilami@msn.com>
2014-07-11 00:57:51 +0200

    Test tweak

--- jqm-all/jqm-engine/src/test/java/com/enioka/jqm/tools/ApiSimpleTest.java
+++ jqm-all/jqm-engine/src/test/java/com/enioka/jqm/tools/ApiSimpleTest.java
@@ -1,6 +1,7 @@
 package com.enioka.jqm.tools;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
 import java.util.ArrayList;
@@ -19,9 +20,12 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import com.enioka.jqm.api.JqmClientFactory;
 import com.enioka.jqm.jpamodel.History;
 import com.enioka.jqm.jpamodel.JobDef;
 import com.enioka.jqm.jpamodel.JobDefParameter;
@@ -31,29 +35,62 @@ import com.enioka.jqm.test.helpers.TestHelpers;
 
 public class ApiSimpleTest extends JqmBaseTest
 {
+    EntityManager em;
+    JqmEngine engine1 = null;
+
+    @Before
+    public void before() throws IOException
+    {
+        jqmlogger.debug("**********************************************************");
+        jqmlogger.debug("Test prepare");
+
+        em = Helpers.getNewEm();
+        TestHelpers.cleanup(em);
+        TestHelpers.createLocalNode(em);
+
+        Helpers.setSingleParam("noHttp", "false", em);
+        Helpers.setSingleParam("useAuth", "false", em);
+
+        File jar = FileUtils.listFiles(new File("../jqm-ws/target/"), new String[] { "war" }, false).iterator().next();
+        FileUtils.copyFile(jar, new File("./webapp/jqm-ws.war"));
+
+        // We need to reset credentials used inside the client
+        JqmClientFactory.resetClient(null);
+
+        // Start the engine
+        engine1 = new JqmEngine();
+        engine1.start("localhost");
+    }
+
+    @After
+    public void after()
+    {
+        jqmlogger.debug("**********************************************************");
+        jqmlogger.debug("Test cleanup");
+
+        em.close();
+        engine1.stop();
+
+        // Java 6 GC being rather inefficient, we must run it multiple times to correctly collect Jetty-created class loaders and avoid
+        // permgen issues
+        System.runFinalization();
+        System.gc();
+        System.runFinalization();
+        System.gc();
+        System.gc();
+    }
+
     @Test
     public void testHttpEnqueue() throws Exception
     {
         jqmlogger.debug("**********************************************************");
         jqmlogger.debug("Starting test testHttpEnqueue");
 
-        File jar = FileUtils.listFiles(new File("../jqm-ws/target/"), new String[] { "war" }, false).iterator().next();
-        FileUtils.copyFile(jar, new File("./webapp/jqm-ws.war"));
-
-        EntityManager em = Helpers.getNewEm();
-        TestHelpers.cleanup(em);
-        TestHelpers.createLocalNode(em);
-        Helpers.setSingleParam("noHttp", "false", em);
-        Helpers.setSingleParam("useAuth", "false", em);
-
         ArrayList<JobDefParameter> jdargs = new ArrayList<JobDefParameter>();
         JobDefParameter jdp = CreationTools.createJobDefParameter("arg", "POUPETTE", em);
         jdargs.add(jdp);
         CreationTools.createJobDef(null, true, "App", jdargs, "jqm-tests/jqm-test-sendmsg/target/test.jar", TestHelpers.qVip, 42,
                 "Marsu-Application", null, "Franquin", "ModuleMachin", "other", "other", true, em);
-
-        JqmEngine engine1 = new JqmEngine();
-        engine1.start("localhost");
 
         em.refresh(TestHelpers.node);
         HttpPost post = new HttpPost("http://" + TestHelpers.node.getDns() + ":" + TestHelpers.node.getPort() + "/ws/simple/ji");
@@ -88,7 +125,6 @@ public class ApiSimpleTest extends JqmBaseTest
         }
 
         TestHelpers.waitFor(1, 10000, em);
-        engine1.stop();
 
         // Check run is OK
         History h = em.createQuery("SELECT j FROM History j", History.class).getSingleResult();
@@ -100,13 +136,7 @@ public class ApiSimpleTest extends JqmBaseTest
     public void testHttpStatus() throws Exception
     {
         jqmlogger.debug("**********************************************************");
-        jqmlogger.debug("Starting test testHttpenqueue");
-
-        EntityManager em = Helpers.getNewEm();
-        TestHelpers.cleanup(em);
-        TestHelpers.createLocalNode(em);
-        Helpers.setSingleParam("noHttp", "false", em);
-        Helpers.setSingleParam("useAuth", "false", em);
+        jqmlogger.debug("Starting test testHttpStatus");
 
         ArrayList<JobDefParameter> jdargs = new ArrayList<JobDefParameter>();
         JobDefParameter jdp = CreationTools.createJobDefParameter("arg", "POUPETTE", em);
@@ -115,9 +145,6 @@ public class ApiSimpleTest extends JqmBaseTest
         @SuppressWarnings("unused")
         JobDef jdDemoMaven = CreationTools.createJobDef(null, true, "App", jdargs, "jqm-tests/jqm-test-sendmsg/target/test.jar",
                 TestHelpers.qVip, 42, "Marsu-Application", null, "Franquin", "ModuleMachin", "other", "other", true, em);
-
-        JqmEngine engine1 = new JqmEngine();
-        engine1.start("localhost");
 
         em.refresh(TestHelpers.node);
         HttpPost post = new HttpPost("http://" + TestHelpers.node.getDns() + ":" + TestHelpers.node.getPort() + "/ws/simple/ji");
@@ -163,7 +190,6 @@ public class ApiSimpleTest extends JqmBaseTest
         IOUtils.copy(in, writer);
         State currentState = State.valueOf(writer.toString());
         IOUtils.closeQuietly(in);
-        engine1.stop();
 
         Assert.assertEquals(State.ENDED, currentState);
     }
