ce30e436cc7627d521ba086d3c29f7bcb9c6f473
marcanpilami <marsu_pilami@msn.com>
2014-07-20 15:55:43 +0200

    Added HTTPS + client certificate support to WS client
    Fixes #127

--- jqm-all/jqm-client/jqm-api-client-jersey/src/main/java/com/enioka/jqm/api/JerseyClient.java
+++ jqm-all/jqm-client/jqm-api-client-jersey/src/main/java/com/enioka/jqm/api/JerseyClient.java
@@ -18,7 +18,12 @@
 
 package com.enioka.jqm.api;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -33,12 +38,16 @@ import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
 
 import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Main JQM client API entry point.
  */
 final class JerseyClient implements JqmClient
 {
+    private static Logger jqmlogger = LoggerFactory.getLogger(JerseyClient.class);
+
     private Properties p;
     private Client client;
     private WebTarget target;
@@ -50,31 +59,33 @@ final class JerseyClient implements JqmClient
     // No public constructor. MUST use factory.
     JerseyClient(Properties p)
     {
-        this.p = p;
-        // Later on, put SSL things here.
-        client = ClientBuilder.newClient();
+        jqmlogger.trace("creating a new WS client");
 
-        // Load conf file if any
+        // /////////////////////////////////////
+        // Property loading
+
+        // Properties priority is: Given explicitly > Given as sys param > Given inside conf file.
+        // They are therefore loaded in that reverse order.
+        this.p = new Properties();
+
+        // Configuration file
         InputStream fis = null;
         try
         {
-            if (System.getProperty("com.enioka.ws.url") != null)
+            fis = this.getClass().getClassLoader().getResourceAsStream("jqm.properties");
+            if (fis != null)
             {
-                p.setProperty("com.enioka.ws.url", System.getProperty("com.enioka.ws.url"));
+                jqmlogger.info("Loading jqm properties file");
+                this.p.load(fis);
             }
             else
             {
-                fis = this.getClass().getClassLoader().getResourceAsStream("jqm.properties");
-                if (fis != null)
-                {
-                    p.load(fis);
-                }
+                jqmlogger.info("No jqm properties file for WS client found in class path");
             }
-
         }
         catch (Exception e)
         {
-            // Ignore invalid files
+            throw new JqmClientException("An error occurred during jqm.properties file search or load", e);
         }
         finally
         {
@@ -88,16 +99,141 @@ final class JerseyClient implements JqmClient
             }
         }
 
-        // Basic Authentication
-        if (this.p.containsKey("com.enioka.jqm.ws.login") && this.p.containsKey("com.enioka.jqm.ws.password"))
+        // System parameter (only for URL, not documented nor supported by the way)
+        if (System.getProperty("com.enioka.ws.url") != null)
         {
+            this.p.setProperty("com.enioka.ws.url", System.getProperty("com.enioka.ws.url"));
+        }
+
+        // Given explicitly
+        this.p.putAll(p);
+
+        // /////////////////////////////////////
+        // SSL certificates
+        if (this.p.containsKey("com.enioka.jqm.ws.truststoreFile"))
+        {
+            jqmlogger.info("A trustore was specified and will be loaded");
+            ClientBuilder bld = ClientBuilder.newBuilder();
+            KeyStore trust = null;
+            InputStream trustIs = null;
+
+            try
+            {
+                trust = KeyStore.getInstance(this.p.getProperty("com.enioka.jqm.ws.truststoreType", "JKS"));
+            }
+            catch (KeyStoreException e)
+            {
+                throw new JqmInvalidRequestException("Specified trust store type ["
+                        + this.p.getProperty("com.enioka.jqm.ws.truststoreType", "JKS") + "] is invalid", e);
+            }
+
+            try
+            {
+                trustIs = new FileInputStream(this.p.getProperty("com.enioka.jqm.ws.truststoreFile"));
+            }
+            catch (FileNotFoundException e)
+            {
+                throw new JqmInvalidRequestException("Trust store file [" + this.p.getProperty("com.enioka.jqm.ws.truststoreFile")
+                        + "] cannot be found", e);
+            }
+
+            String trustp = this.p.getProperty("com.enioka.jqm.ws.truststorePass", null);
+            try
+            {
+                trust.load(trustIs, (trustp == null ? null : trustp.toCharArray()));
+            }
+            catch (Exception e)
+            {
+                throw new JqmInvalidRequestException("Could not load the trust store file", e);
+            }
+            finally
+            {
+                try
+                {
+                    trustIs.close();
+                }
+                catch (IOException e)
+                {
+                    // Nothing to do.
+                }
+            }
+
+            bld.trustStore(trust);
+
+            // Client certificate
+            if (this.p.containsKey("com.enioka.jqm.ws.keystoreFile"))
+            {
+                jqmlogger.info("A keystore was specified and will be loaded");
+                KeyStore keyStore = null;
+                InputStream keyIs = null;
+
+                try
+                {
+                    keyStore = KeyStore.getInstance(this.p.getProperty("com.enioka.jqm.ws.keystoreType", "JKS"));
+                }
+                catch (KeyStoreException e)
+                {
+                    throw new JqmInvalidRequestException("Specified key store type ["
+                            + this.p.getProperty("com.enioka.jqm.ws.keystoreType", "JKS") + "] is invalid", e);
+                }
+
+                try
+                {
+                    keyIs = new FileInputStream(this.p.getProperty("com.enioka.jqm.ws.keystoreFile"));
+                }
+                catch (FileNotFoundException e)
+                {
+                    throw new JqmInvalidRequestException("Key store file [" + this.p.getProperty("com.enioka.jqm.ws.keystoreFile")
+                            + "] cannot be found", e);
+                }
+
+                String keyp = this.p.getProperty("com.enioka.jqm.ws.keystorePass", null);
+                try
+                {
+                    keyStore.load(keyIs, (keyp == null ? null : keyp.toCharArray()));
+                }
+                catch (Exception e)
+                {
+                    throw new JqmInvalidRequestException("Could not load the key store file", e);
+                }
+                finally
+                {
+                    try
+                    {
+                        keyIs.close();
+                    }
+                    catch (IOException e)
+                    {
+                        // Nothing to do.
+                    }
+                }
+
+                bld.keyStore(keyStore, keyp);
+            }
+
+            client = bld.build();
+        }
+        else
+        {
+            // No trust store given = no SSL allowed => default client
+            client = ClientBuilder.newClient();
+        }
+
+        // Basic Authentication (only allowed when no client certificate is given)
+        if (this.p.containsKey("com.enioka.jqm.ws.login") && this.p.containsKey("com.enioka.jqm.ws.password")
+                && !this.p.containsKey("com.enioka.jqm.ws.keystoreFile"))
+        {
+            jqmlogger.info("A login/password pair was specified and will be used");
             HttpAuthenticationFeature auth = HttpAuthenticationFeature.basic(this.p.getProperty("com.enioka.jqm.ws.login"),
                     this.p.getProperty("com.enioka.jqm.ws.password"));
             client.register(auth);
         }
 
-        // Set properties
-        this.target = client.target(this.p.getProperty("com.enioka.jqm.ws.url", "http://localhost:1789/api/ws"));
+        // /////////////////////////////////////
+        // Create client
+        String url = this.p.getProperty("com.enioka.jqm.ws.url", "http://localhost:1789/ws/client");
+        jqmlogger.debug("The following root service URL will be used: " + url);
+        this.target = client.target(url);
     }
 
     @Override
---
+++ jqm-all/jqm-client/jqm-api-client-jersey/src/test/java/com/enioka/jqm/tools/SslTest.java
@@ -0,0 +1,113 @@
+package com.enioka.jqm.tools;
+
+import java.io.File;
+import java.util.List;
+import java.util.Properties;
+
+import javax.naming.NamingException;
+import javax.naming.spi.NamingManager;
+import javax.persistence.EntityManager;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.enioka.jqm.api.JqmClientFactory;
+import com.enioka.jqm.api.Queue;
+import com.enioka.jqm.jpamodel.Node;
+import com.enioka.jqm.jpamodel.RRole;
+import com.enioka.jqm.pki.JpaCa;
+import com.enioka.jqm.test.helpers.CreationTools;
+import com.enioka.jqm.test.helpers.TestHelpers;
+
+public class SslTest
+{
+    public static Logger jqmlogger = Logger.getLogger(SslTest.class);
+
+    JqmEngine engine1;
+    EntityManager em;
+    public static org.hsqldb.Server s;
+    private Properties p;
+
+    @BeforeClass
+    public static void testInit() throws Exception
+    {
+        JndiContext.createJndiContext();
+        s = new org.hsqldb.Server();
+        s.setDatabaseName(0, "testdbengine");
+        s.setDatabasePath(0, "mem:testdbengine");
+        s.setLogWriter(null);
+        s.setSilent(true);
+        s.start();
+    }
+
+    @AfterClass
+    public static void stop() throws NamingException
+    {
+        JqmClientFactory.resetClient(null);
+        Helpers.resetEmf();
+        ((JndiContext) NamingManager.getInitialContext(null)).resetSingletons();
+        s.shutdown();
+        s.stop();
+    }
+
+    @Test
+    public void testSslAuth() throws Exception
+    {
+        jqmlogger.debug("**********************************************************");
+        jqmlogger.debug("Starting test testSslAuth");
+
+        // Test envt
+        em = Helpers.getNewEm();
+        TestHelpers.cleanup(em);
+        TestHelpers.createLocalNode(em);
+
+        // Start in SSL mode with web services
+        File jar = FileUtils.listFiles(new File("../../jqm-ws/target/"), new String[] { "war" }, false).iterator().next();
+        FileUtils.copyFile(jar, new File("./webapp/jqm-ws.war"));
+        Helpers.setSingleParam("noHttp", "false", em);
+        Helpers.setSingleParam("useAuth", "true", em);
+        Helpers.setSingleParam("useSsl", "true", em);
+
+        em.getTransaction().begin();
+        TestHelpers.node.setLoadApiAdmin(true);
+        TestHelpers.node.setLoadApiClient(true);
+        TestHelpers.node.setLoapApiSimple(true);
+        em.getTransaction().commit();
+
+        engine1 = new JqmEngine();
+        engine1.start("localhost");
+
+        // Test user
+        RRole r = em.createQuery("SELECT rr from RRole rr WHERE rr.name = :r", RRole.class).setParameter("r", "administrator")
+                .getSingleResult();
+        CreationTools.createUser(em, "test", "test", r);
+
+        // Create auth data
+        Node n = em.find(Node.class, TestHelpers.node.getId());
+        em.refresh(n);
+        JpaCa.prepareClientStore(em, "CN=test", "./conf/client.pfx", "SuperPassword", "client-cert", "./conf/client.cer");
+
+        // Configure client
+        p = new Properties();
+        p.put("com.enioka.jqm.ws.url", "https://" + n.getDns() + ":" + n.getPort() + "/ws/client");
+        p.put("com.enioka.jqm.ws.login", "testdfg"); // Wrong on purpose...
+        p.put("com.enioka.jqm.ws.password", "testcdfg"); // Same as above
+        p.put("com.enioka.jqm.ws.keystoreFile", "./conf/client.pfx");
+        p.put("com.enioka.jqm.ws.keystorePass", "SuperPassword");
+        p.put("com.enioka.jqm.ws.keystoreType", "PKCS12");
+        p.put("com.enioka.jqm.ws.truststoreFile", "./conf/trusted.jks");
+        p.put("com.enioka.jqm.ws.truststorePass", "SuperPassword");
+        JqmClientFactory.setProperties(p);
+
+        // Try a query
+        List<Queue> qs = JqmClientFactory.getClient().getQueues();
+        Assert.assertEquals(9, qs.size());
+
+        // Done
+        engine1.stop();
+    }
+}
