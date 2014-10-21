3ca943ca60abc5a7c22901bbfd24876b0deec5eb
marcanpilami <marsu_pilami@msn.com>
2014-10-21 17:13:23 +0200

    Added permission script. Closes #65

--- jqm-all/jqm-engine/src/main/scripts/jqm.sh
+++ jqm-all/jqm-engine/src/main/scripts/jqm.sh
@@ -2,6 +2,23 @@
 # SRE - 2014
 
 ##############################
+##### SUDO                ####
+##############################
+
+if [ $(whoami) = "root" ]
+then
+    if [ "x${JQM_SERVICE_ACCOUNT}" = "x" ]
+    then        
+        echo "Defaulting to account jqm for running the script. Set JQM_SERVICE_ACCOUNT to use a different account."
+        JQM_SERVICE_ACCOUNT="jqm"
+    fi
+    echo "Re-launching script as ${JQM_SERVICE_ACCOUNT}"
+    su - ${JQM_SERVICE_ACCOUNT} -c "export JQM_NODE=\${JQM_NODE:-${JQM_NODE}}; $(pwd -P)/$(basename $0) $*"
+    exit $?
+fi
+
+
+##############################
 ##### Check and setup Env ####
 ##############################
 
@@ -13,26 +30,34 @@ cd $(dirname $0)
 JQM_JAR="jqm.jar"  # Name of JQM Java archive file
 WAITING_TIME=70    # Seconds to wait for JQM to shutdown gracefully
 
-if [[ $JQM_NODE == "" ]]
+if [ "x${JQM_NODE}" = "x" ]
 then
-	echo "Defaulting to $USER as node name. Set JQM_NODE to use another node name"
-	JQM_NODE=$USER
+ echo "Defaulting to $USER as node name. Set JQM_NODE to use another node name"
+ JQM_NODE=$USER
 fi
 
 # Java extra options that can be used to customise memory settings
 JAVA_OPTS=${JAVA_OPTS:--Xms160m -Xmx512m -XX:MaxPermSize=128m}
 
-
-JAVA="${JAVA_HOME:-/usr/java6_64/jre/}/bin/java $JAVA_OPTS"
-$JAVA -version  > /dev/null 2>&1
-if [[ $? -ne 0 ]]
+if [ ! "x${JAVA_HOME}" = "x" ]
 then
-        echo "No java found. Please define JAVA_HOME"
+ JAVA=${JAVA_HOME}/bin/java
+else
+ JAVA=$(which java 2>/dev/null)
+ if [ $? -ne 0 ]
+ then
+  JAVA="/usr/java6_64/jre/bin/java"
+ fi
+fi
+JAVA="${JAVA} ${JAVA_OPTS}"
+$JAVA -version  > /dev/null 2>&1
+if [ $? -ne 0 ]
+then
+        echo "No java found. Please define JAVA_HOME or put java inside PATH"
         exit 1
 fi
 
-
-JQM_PID_FILE=jqm_${JQM_NODE}.pid
+JQM_PID_FILE=tmp/jqm_${JQM_NODE}.pid
 JQM_LOG_OUT_FILE=logs/jqm_${JQM_NODE}_out
 JQM_LOG_ERR_FILE=logs/jqm_${JQM_NODE}_err
 STDOUT_NPIPE=logs/stdout_$$.pipe
@@ -42,7 +67,7 @@ JQM_LOG_HISTORY=30  # Days of log history to keep
 # Helper function to log output with rotation on time basis (per hour)
 # Arg 1 is log file prefix
 log_rotate() {
-	while read i
+ while read i
     do
         echo "$i" >> $1_$(date +%Y.%m.%d-%H).log
     done
@@ -50,129 +75,135 @@ log_rotate() {
 
 # Helper function to cleanup log pipes
 remove_npipes() {
-	rm logs/stdout_*.pipe logs/stderr_*.pipe > /dev/null 2>&1
+ rm logs/stdout_*.pipe logs/stderr_*.pipe > /dev/null 2>&1
 }
+
 
 #############################################
 #### Start/Stop/Restart/Status functions ####
 #############################################
 
 jqm_start() {
-	echo Starting JQM...
-	test -e ${JQM_PID_FILE}
-        if [[ $? -eq 0 ]]
+ echo Starting JQM...
+ test -e ${JQM_PID_FILE}
+        if [ $? -eq 0 ]
         then
                 echo "PID file found (${JQM_PID_FILE})."
-		JQM_PID=$(cat ${JQM_PID_FILE})
-        	ps -p $JQM_PID > /dev/null 2>&1
-        	if [[ $? -ne 0 ]]
-        	then
-                	echo "PID file is here (${JQM_PID_FILE}) but JQM daemon is gone..."
-                	echo "Cleaning up pid file"
-                	rm ${JQM_PID_FILE}
-        	else
-               		echo "JQM is already running with PID ${JQM_PID} for node ${JQM_NODE}"
-                	exit 1
-        	fi
+  JQM_PID=$(cat ${JQM_PID_FILE})
+         ps -p $JQM_PID > /dev/null 2>&1
+         if [ $? -ne 0 ]
+         then
+                 echo "PID file is here (${JQM_PID_FILE}) but JQM daemon is gone..."
+                 echo "Cleaning up pid file"
+                 rm ${JQM_PID_FILE}
+         else
+                 echo "JQM is already running with PID ${JQM_PID} for node ${JQM_NODE}"
+                 exit 1
+         fi
         fi
-	# Remove old logs
-	for JQM_LOG_FILE in $JQM_LOG_OUT_FILE $JQM_LOG_ERR_FILE
-	do
-		find logs -name "$(basename ${JQM_LOG_FILE})*.log" -mtime +${JQM_LOG_HISTORY} | xargs -r rm
-	done
-	# We can go on...
-	if [[ $1 == "console" ]]
-	then
-		$JAVA -jar $JQM_JAR -startnode $JQM_NODE
-	else
-		remove_npipes
-		mknod $STDOUT_NPIPE p
-		mknod $STDERR_NPIPE p
-		log_rotate <$STDOUT_NPIPE $JQM_LOG_OUT_FILE &
-		log_rotate <$STDERR_NPIPE $JQM_LOG_ERR_FILE &
-		exec 1> $STDOUT_NPIPE
-		exec 2> $STDERR_NPIPE
-		nohup $JAVA -jar $JQM_JAR -startnode $JQM_NODE &
-		JQM_PID=$!
-		echo $JQM_PID > ${JQM_PID_FILE}
-		echo "JQM Started with pid ${JQM_PID}"
-	fi
+ # Remove old logs
+ for JQM_LOG_FILE in $JQM_LOG_OUT_FILE $JQM_LOG_ERR_FILE
+ do
+  find logs -name "$(basename ${JQM_LOG_FILE})*.log" -mtime +${JQM_LOG_HISTORY} -exec rm {} \;
+ done
+ # We can go on...
+ if [ "$1" = "console" ]
+ then
+  truss $JAVA -jar $JQM_JAR -startnode $JQM_NODE
+ else
+  remove_npipes
+  mknod $STDOUT_NPIPE p
+  mknod $STDERR_NPIPE p
+  log_rotate <$STDOUT_NPIPE $JQM_LOG_OUT_FILE &
+  log_rotate <$STDERR_NPIPE $JQM_LOG_ERR_FILE &
+  exec 1> $STDOUT_NPIPE
+  exec 2> $STDERR_NPIPE
+  nohup $JAVA -jar $JQM_JAR -startnode $JQM_NODE &
+  JQM_PID=$!
+  echo $JQM_PID > ${JQM_PID_FILE}
+  echo "JQM Started with pid ${JQM_PID}"
+ fi
 }
 
 jqm_stop() {
-	echo Stopping JQM...
-	test -e ${JQM_PID_FILE}
-	if [[ $? -ne 0 ]]
-	then
-        	echo "PID file not found (${JQM_PID_FILE})."
-		echo "Here are all jqm engine running on this host, choose the good one and kill it yourself:"
-		ps -ef |grep $JQM_JAR | grep -v grep
-        	exit 1
-	fi
-	JQM_PID=$(cat ${JQM_PID_FILE})
-	echo "Sending SIGTERM to process $JQM_PID and waiting for graceful shutdown."
-	GRACEFUL_KILL="no"
-	kill $JQM_PID
-	while [ $(( WAITING_TIME -= 1 )) -ge 0 ]
-	do
-		printf "."
-		ps -p $JQM_PID > /dev/null 2>&1
-		if [[ $? -ne 0 ]]
-		then
-			GRACEFUL_KILL="yes"
-			break;
-		fi
-		sleep 1
-	done
-	echo ""
-	if [[ $GRACEFUL_KILL == "no" ]]
-	then
-		echo "JQM Engine did not respond to SIGTERM. Killing (SIGKILL) JQM Engine..."
-		kill -9 $JQM_PID
-	else
-		echo "JQM engine shutdown properly."
-	fi
-	rm ${JQM_PID_FILE}
-	remove_npipes
+ echo Stopping JQM...
+ test -e ${JQM_PID_FILE}
+ if [ $? -ne 0 ]
+ then
+         echo "PID file not found (${JQM_PID_FILE})."
+  echo "Here are all jqm engines running on this host, choose the good one and kill it yourself:"
+  ps -ef |grep $JQM_JAR | grep -v grep
+         exit 1
+ fi
+ JQM_PID=$(cat ${JQM_PID_FILE})
+ echo "Sending SIGTERM to process $JQM_PID and waiting for graceful shutdown."
+ GRACEFUL_KILL="no"
+ kill $JQM_PID
+ while [ $(( WAITING_TIME -= 1 )) -ge 0 ]
+ do
+  printf "."
+  ps -p $JQM_PID > /dev/null 2>&1
+  if [ $? -ne 0 ]
+  then
+   GRACEFUL_KILL="yes"
+   break;
+  fi
+  sleep 1
+ done
+ echo ""
+ if [ $GRACEFUL_KILL = "no" ]
+ then
+  echo "JQM Engine did not respond to SIGTERM. Killing (SIGKILL) JQM Engine..."
+  kill -9 $JQM_PID
+ else
+  echo "JQM engine shutdown properly."
+ fi
+ rm ${JQM_PID_FILE}
+ remove_npipes
 }
 
 jqm_status() {
-	test -e ${JQM_PID_FILE}
-	if [[ $? -ne 0 ]]
-	then
-       	echo "PID file not found (${JQM_PID_FILE})."
-		echo "Here are all jqm engine running on this host"
-		ps -ef |grep $JQM_JAR | grep -v grep
-       	exit 1
-	fi
-	JQM_PID=$(cat ${JQM_PID_FILE})
-	ps -p $JQM_PID > /dev/null 2>&1
-	if [[ $? -ne 0 ]]
-	then
-		echo "PID file is here (${JQM_PID_FILE}) but JQM daemon is gone..."
-		echo "Cleaning up pid file"
-		rm ${JQM_PID_FILE}
-		remove_npipes
-		exit 1
-	else
-		echo "JQM is running with PID ${JQM_PID}"
-	fi
+ test -e ${JQM_PID_FILE}
+ if [ $? -ne 0 ]
+ then
+        echo "PID file not found (${JQM_PID_FILE})."
+  echo "Here are all jqm engines running on this host"
+  ps -ef |grep $JQM_JAR | grep -v grep
+        exit 1
+ fi
+ JQM_PID=$(cat ${JQM_PID_FILE})
+ ps -p $JQM_PID > /dev/null 2>&1
+ if [ $? -ne 0 ]
+ then
+  echo "PID file is here (${JQM_PID_FILE}) but JQM daemon is gone..."
+  echo "Cleaning up pid file"
+  rm ${JQM_PID_FILE}
+  remove_npipes
+  exit 1
+ else
+  echo "JQM is running with PID ${JQM_PID}"
+ fi
 }
 
 jqm_createnode() {
-	$JAVA -jar $JQM_JAR -createnode $JQM_NODE
+ $JAVA -jar $JQM_JAR -createnode $JQM_NODE
 }
 
 jqm_enqueue() {
-	$JAVA -jar $JQM_JAR -enqueue $1
+ $JAVA -jar $JQM_JAR -enqueue $1
 }
 
 jqm_import_xml() {
-	$JAVA -jar $JQM_JAR -importjobdef $1
+ $JAVA -jar $JQM_JAR -importjobdef $1
 }
 
 jqm_import_all_xml() {
-	$JAVA -jar $JQM_JAR -importjobdef $(find jobs -name "*xml" -type f | tr "\\n" ",")
+ $JAVA -jar $JQM_JAR -importjobdef $(find jobs -name "*xml" -type f | tr "\\n" ",")
+}
+
+jqm_enable_gui() {
+ $JAVA -jar $JQM_JAR -w enable
+ $JAVA -jar $JQM_JAR -r "$1"
 }
 
 
@@ -180,35 +211,38 @@ jqm_import_all_xml() {
 ##### Decide what to do... ####
 ###############################
 case "$ACTION" in
-	start)
-		jqm_start
-		;;
-	startconsole)
-		jqm_start "console"
-		;;
-	stop)
-		jqm_stop
-		;;
-	restart)
-		jqm_stop
-		jqm_start
-		;;
-	status)
-		jqm_status
-		;;
-	createnode)
-		jqm_createnode
-		;;
-	enqueue|run|execute)
-		jqm_enqueue $2
-		;;
-	importxml|import|xml)
-		jqm_import_xml $2
-		;;
-	importallxml|allxml)
-		jqm_import_all_xml
-		;;
-	*)
-		echo "Usage: $0 {start|stop|restart|status|createnode|importxml|allxml|enqueue}"
-		;;
+ start)
+  jqm_start
+  ;;
+ startconsole)
+  jqm_start "console"
+  ;;
+ stop)
+  jqm_stop
+  ;;
+ restart)
+  jqm_stop
+  jqm_start
+  ;;
+ status)
+  jqm_status
+  ;;
+ createnode)
+  jqm_createnode
+  ;;
+ enqueue|run|execute)
+  jqm_enqueue $2
+  ;;
+ importxml|import|xml)
+  jqm_import_xml $2
+  ;;
+ importallxml|allxml)
+  jqm_import_all_xml
+  ;;
+ enablegui)
+  jqm_enable_gui "$2"
+  ;;
+ *)
+  echo "Usage: $0 {start|stop|restart|status|createnode|importxml|allxml|enqueue <jobname>|startconsole|enablegui <rootpassword>}"
+  ;;
 esac
---
+++ jqm-all/jqm-engine/src/main/scripts/permissions.sh
@@ -0,0 +1,108 @@
+#!/bin/sh
+
+## JQM_ADMIN_ACCOUNT - default jqmadm - owns the files and may install new batchs.
+## JQM_EXEC_GROUP - default jqm - group of users which are able to run the engine. It can only launch the engine, not modify it.
+## JQM_SERVICE_ACCOUNT - default jqm - is the service account. It's main group is JQM_EXEC_GROUP.
+##
+## Remember to set umask 107 for JQM_SERVICE_ACCOUNT for better protection.
+
+## Go to JQM_ROOT
+cd $(dirname $0)/..
+
+## Get the accounts and groups
+if [ $(whoami) = "root" ]
+then
+    echo "Defaulting to account jqmadm for running the script. Set JQM_ADMIN_ACCOUNT to use a different account."
+    su - ${JQM_ADMIN_ACCOUNT:-jqmadm} -c $(pwd -P)/$0 $*
+fi
+
+## This scripts runs as the admin account
+JQM_ADMIN_ACCOUNT=$(whoami)
+JQM_SERVICE_ACCOUNT=${JQM_SERVICE_ACCOUNT:-"jqm"}
+JQM_EXEC_GROUP=${JQM_EXEC_GROUP:-"jqm"}
+
+echo "Admin account is: \t\t\t${JQM_ADMIN_ACCOUNT} \t(current account)"
+echo "Service account is: \t\t\t${JQM_SERVICE_ACCOUNT} \t(set variable JQM_SERVICE_ACCOUNT to change this)"
+echo "Service account is inside group: \t${JQM_EXEC_GROUP} \t(set variable JQM_EXEC_GROUP to change this)"
+
+## Check users & groups
+id | grep ${JQM_EXEC_GROUP} 2>&1 >/dev/null
+if [ $? -ne 0 ]
+then
+    echo "Admin account ${JQM_ADMIN_ACCOUNT} must be member of group ${JQM_EXEC_GROUP} to continue"
+    exit 1
+fi
+
+grep ${JQM_SERVICE_ACCOUNT} /etc/passwd 2>&1 >/dev/null
+if [ $? -ne 0 ]
+then
+    echo "User ${JQM_SERVICE_ACCOUNT} does not exist"
+    exit 1
+fi
+
+grep ${JQM_EXEC_GROUP} /etc/group 2>&1 >/dev/null
+if [ $? -ne 0 ]
+then
+    echo "Group ${JQM_EXEC_GROUP} does not exist"
+    exit 1
+fi
+
+grep ${JQM_EXEC_GROUP} /etc/group | grep ${JQM_SERVICE_ACCOUNT} 2>&1 >/dev/null
+if [ $? -ne 0 ]
+then
+    echo "User ${JQM_SERVICE_ACCOUNT} is not inside group ${JQM_EXEC_GROUP}"
+    exit 1
+fi
+
+## Chown
+chmod -R 700 .
+chown -R ${JQM_ADMIN_ACCOUNT}:${JQM_EXEC_GROUP} .
+
+# directory
+chmod 750 .
+
+# bin contains this script
+chmod 700 bin
+chmod 700 bin/*.sh
+
+# conf contains database connexion passwords, modified by the admin.
+chmod 750 conf
+chmod 640 conf/*
+
+# db is only used with hsqldb, and contains the database which is modified by the engine.
+chmod 770 db 
+chmod 660 db/* 2>/dev/null
+
+# ext contains libraries contained inside the payload lib path. Modified by installing new payloads.
+chmod 750 ext
+chmod 640 ext/*
+
+# jobs are the payloads. Modified by installing new payloads.
+chmod 750 jobs
+find jobs -type d -exec chmod 750 {} \;
+find jobs -type f -exec chmod 640 {} \;
+
+# lib is the engine and its dependencies
+chmod 750 lib
+chmod 640 lib/*.jar
+
+# logs contains both engine logs and payload logs. May contain sensitive data
+chmod 770 logs
+chmod 660 logs/* 2>/dev/null
+
+# outputfiles are files created by payloads. May contain sensitive data
+chmod 770 outputfiles
+find outputfiles -type d -exec chmod 770 {} \;
+find outputfiles -type f -exec chmod 660 {} \;
+
+# tmp is a workspace for payloads. May also contain sensitive data
+chmod 770 tmp
+chmod 660 tmp/* 2>/dev/null
+
+# webapp is loaded by the engine as a web app.
+chmod 770 webapp
+chmod 640 webapp/*.war
+
+# single files
+chmod 640 *.jar 
+chmod 750 *.sh *.ps1
