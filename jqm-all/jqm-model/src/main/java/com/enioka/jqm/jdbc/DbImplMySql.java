ae5e0bcafed86fe2a9579b492130f5be3652c1ec
Marc-Antoine Gouillart <marsu_pilami@msn.com>
2017-05-29 17:24:55 +0200

    Another MySQL oddity.
    Signed-off-by: Marc-Antoine Gouillart <marsu_pilami@msn.com>

--- jqm-all/jqm-model/src/main/java/com/enioka/jqm/jdbc/DbImplMySql.java
+++ jqm-all/jqm-model/src/main/java/com/enioka/jqm/jdbc/DbImplMySql.java
@@ -48,11 +48,12 @@ public class DbImplMySql implements DbAdapter
             return "";
         }
         return sql.replace("MEMORY TABLE", "TABLE").replace("JQM_PK.nextval", "?").replace(" DOUBLE", " DOUBLE PRECISION")
-                .replace("UNIX_MILLIS()", "UNIX_TIMESTAMP()").replace("IN(UNNEST(?))", "IN(?)")
+                .replace("UNIX_MILLIS()", "ROUND(UNIX_TIMESTAMP(CURTIME(4)) * 1000)").replace("IN(UNNEST(?))", "IN(?)")
                 .replace("CURRENT_TIMESTAMP - 1 MINUTE", "(UNIX_TIMESTAMP() - 60)")
                 .replace("CURRENT_TIMESTAMP - ? SECOND", "(NOW() - INTERVAL ? SECOND)").replace("FROM (VALUES(0))", "FROM DUAL")
                 .replace("DNS||':'||PORT", "CONCAT(DNS, ':', PORT)").replace(" TIMESTAMP ", " TIMESTAMP(3) ")
-                .replace("CURRENT_TIMESTAMP", "FFFFFFFFFFFFFFFFF@@@@").replace("FFFFFFFFFFFFFFFFF@@@@", "CURRENT_TIMESTAMP(3)");
+                .replace("CURRENT_TIMESTAMP", "FFFFFFFFFFFFFFFFF@@@@").replace("FFFFFFFFFFFFFFFFF@@@@", "CURRENT_TIMESTAMP(3)")
+                .replace("TIMESTAMP(3) NOT NULL", "TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3)");
     }
 
     @Override
