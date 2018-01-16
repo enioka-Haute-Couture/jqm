59e6a27a0d6257ba9e4a1dcd1a75f89e11ede028
marcanpilami <marsu_pilami@msn.com>
2018-01-16 18:32:38 +0100

    Full Windows 1709 Docker build with nexus cache container
    Signed-off-by: marcanpilami <marsu_pilami@msn.com>

--- docker/windows/Update-Images.ps1
+++ docker/windows/Update-Images.ps1
@@ -1,7 +1,24 @@
-﻿#docker build --pull --rm -t "jqm/openjdk:latest" -t "jqm/openjdk:8u151-jdk-nanoserver-sac-1709" -f $PSScriptRoot\DockerFile.jdk .
-#docker build --rm -t "jqm/openjdk:latest-jre" -f DockerFile.jre .
-#docker build --rm -t "jqm/maven:latest" -f $PSScriptRoot\DockerFile.maven .
+﻿Set-Location $PSScriptRoot
 
-docker build --rm -t "jqm:latest" -f $PSScriptRoot\DockerFile $PSScriptRoot/../..
+# Nexus must be started before build.
+if ( -not (docker volume ls | select-string -SimpleMatch -Quiet "nexus-data"))
+{
+    docker volume create --name=nexus-data
+}
+docker-compose up --build -d
+
+$r = Invoke-WebRequest "http://$env:COMPUTERNAME`:8081" -UseBasicParsing -DisableKeepAlive -Method Head -ErrorAction SilentlyContinue
+while ($r = $null -or $r.statuscode -ne 200)
+{
+    Start-Sleep -Seconds 1
+    $r = Invoke-WebRequest "http://$env:COMPUTERNAME`:8081" -UseBasicParsing -DisableKeepAlive -Method Head -ErrorAction SilentlyContinue
+}
+
+# All helpers must be built before build.
+docker-compose --project-directory ./java build
+docker-compose --project-directory ./maven build
+
+# Now build JQM itself
+docker build --rm -t "enioka/jqm:latest" --build-arg "MVN_SETTINGS=-s settings.xml" --network windows_default -f $PSScriptRoot\jqm\DockerFile $PSScriptRoot/../..
 
 #  docker run -it --rm -p 1789:1789 jqm
