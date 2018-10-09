b33ff7676faf7136b9e0415cd9786a733d81a510
Marc-Antoine Gouillart <marsu_pilami@msn.com>
2018-10-09 18:12:00 +0200

    Moved index template

--- jqm-all/jqm-ws/webpack.common.js
+++ jqm-all/jqm-ws/webpack.common.js
@@ -3,46 +3,46 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-	entry: './src/main/angular/app.js',
-	plugins: [
-		new CleanWebpackPlugin(['dist']),
-		new HtmlWebpackPlugin({
-			title: 'JQM administration',
-			template: 'src/main/angular/template/index.html',
-			inject: false,
-			favicon: 'src/main/angular/css/favicon.png',
-			filename: '../index.html'
-		})],
-	output: {
-		filename: '[name].bundle.[contenthash].js',
-		path: path.resolve(__dirname, 'src/main/webapp/dist'),
-		publicPath: '/dist/'
-	},
-	module: {
-		rules: [{
-			test: /\.css$/,
-			use: ['style-loader', 'css-loader']
-		}, {
-			test: /\.(png|svg|jpg|gif)$/,
-			use: ['file-loader']
-		}, {
-			test: /\.(woff|woff2|eot|ttf|otf)$/,
-			use: ['file-loader']
-		}, {
-			test: /components.*\.html$/,
-			use: ['raw-loader']
-		},]
-	},
-	optimization: {
-		runtimeChunk: 'single',
-		splitChunks: {
-			cacheGroups: {
-				vendor: {
-					test: /[\\/]node_modules[\\/]/,
-					name: 'vendors',
-					chunks: 'all'
-				}
-			}
-		}
-	}
+    entry: './src/main/angular/app.js',
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'JQM administration',
+            template: 'src/main/angular/index.wptemplate.html',
+            inject: false,
+            favicon: 'src/main/angular/css/favicon.png',
+            filename: '../index.html'
+        })],
+    output: {
+        filename: '[name].bundle.[contenthash].js',
+        path: path.resolve(__dirname, 'src/main/webapp/dist'),
+        publicPath: '/dist/'
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
+        }, {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: ['file-loader']
+        }, {
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
+            use: ['file-loader']
+        }, {
+            test: /components.*\.html$/,
+            use: ['raw-loader']
+        },]
+    },
+    optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all'
+                }
+            }
+        }
+    }
 };
