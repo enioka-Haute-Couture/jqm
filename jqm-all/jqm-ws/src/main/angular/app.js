16da1ff029404a9f0d3262edee73c4479ac83c8c
Marc-Antoine Gouillart <marsu_pilami@msn.com>
2018-10-13 15:50:23 +0200

    GUI: now using recent HTML convention for user tags

--- jqm-all/jqm-ws/src/main/angular/app.js
+++ jqm-all/jqm-ws/src/main/angular/app.js
@@ -11,33 +11,34 @@ import './css/app.css';
 
 var jqmApp = angular.module('jqmApp', [jqmComponentsModule, ngRoute, ngSanitize]);
 
-jqmApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-  $locationProvider.hashPrefix('!');
+jqmApp.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider)
+{
+    $locationProvider.hashPrefix('!');
 
-  $routeProvider.when('/', {
-    template: '<home></home>'
-  }).when('/home', {
-    template: '<home></home>'
-  }).when('/node', {
-    template: '<nodes></nodes>'
-  }).when('/q', {
-    template: '<queues></queues>'
-  }).when('/jndi', {
-    template: '<jndi></jndi>'
-  }).when('/prm', {
-    template: '<prms></prms>'
-  }).when('/jd', {
-    template: '<jds></jds>'
-  }).when('/user', {
-    template: '<users></users>'
-  }).when('/role', {
-    template: '<roles></roles>'
-  }).when('/history', {
-    template: '<history></history>'
-  }).when('/qmapping', {
-    template: '<mappings></mappings>'
-  }).otherwise({
-    redirectTo: '/'
-  });
+    $routeProvider.when('/', {
+        template: '<jqm-home></jqm-home>'
+    }).when('/home', {
+        template: '<jqm-home></jqm-home>'
+    }).when('/node', {
+        template: '<jqm-nodes></jqm-nodes>'
+    }).when('/q', {
+        template: '<jqm-queues></jqm-queues>'
+    }).when('/jndi', {
+        template: '<jqm-jndi></jqm-jndi>'
+    }).when('/prm', {
+        template: '<jqm-prms></jqm-prms>'
+    }).when('/jd', {
+        template: '<jqm-jds></jqm-jds>'
+    }).when('/user', {
+        template: '<jqm-users></jqm-users>'
+    }).when('/role', {
+        template: '<jqm-roles></jqm-roles>'
+    }).when('/history', {
+        template: '<jqm-history></jqm-history>'
+    }).when('/qmapping', {
+        template: '<jqm-mappings></jqm-mappings>'
+    }).otherwise({
+        redirectTo: '/'
+    });
 }
 ]);
