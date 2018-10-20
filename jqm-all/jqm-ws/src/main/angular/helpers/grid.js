da37d57cee52114860955fc22af7de0792eef3a0
Marc-Antoine Gouillart <marsu_pilami@msn.com>
2018-10-20 20:14:48 +0200

    GUI: fixed checkbow columns

--- jqm-all/jqm-ws/src/main/angular/helpers/grid.js
+++ jqm-all/jqm-ws/src/main/angular/helpers/grid.js
@@ -6,57 +6,57 @@ import uigrid from 'angular-ui-grid/ui-grid.js';
 const xxx = angular.module('jqm.editors', ['ui.grid']);
 
 xxx.constant('jqmCellTemplateBoolean',
-	'<div class="ui-grid-cell-contents"><span class="glyphicon {{ row.entity[col.field] ? \'glyphicon-ok\' : \'glyphicon-none\' }}"></span></div>');
+    '<div class="ui-grid-cell-contents"><span class="far {{ row.entity[col.field] ? \'fa-check-square\' : \'fa-square\' }}"></span></div>');
 xxx.constant('jqmCellEditorTemplateBoolean',
-	'<div><input jqm-grid-boolean-editor class="ui-grid-cell-contents" type="checkbox" ng-model="MODEL_COL_FIELD"/></div>');
+    '<div><input jqm-grid-boolean-editorX class="ui-grid-cell-contents" type="checkbox" ng-model="MODEL_COL_FIELD"/></div>');
 
 xxx.directive('jqmGridBooleanEditor', ['uiGridEditConstants', '$timeout', function (uiGridEditConstants, $timeout)
 {
-	return {
-		scope: true,
-		require: ['?^uiGrid', '?^uiGridRenderContainer', 'ngModel'],
-		compile: function ()
-		{
-			return {
-				pre: function ($scope, $elm, $attrs)
-				{
-				},
+    return {
+        scope: true,
+        require: ['?^uiGrid', '?^uiGridRenderContainer', 'ngModel'],
+        compile: function ()
+        {
+            return {
+                pre: function ($scope, $elm, $attrs)
+                {
+                },
 
-				post: function ($scope, $elm, $attrs, controllers)
-				{
-					var uiGridCtrl, renderContainerCtrl, ngModel;
-					if (controllers[0])
-					{
-						uiGridCtrl = controllers[0];
-					}
-					if (controllers[1])
-					{
-						renderContainerCtrl = controllers[1];
-					}
-					if (controllers[2])
-					{
-						ngModel = controllers[2];
-					}
-					var grid = uiGridCtrl.grid;
+                post: function ($scope, $elm, $attrs, controllers)
+                {
+                    var uiGridCtrl, renderContainerCtrl, ngModel;
+                    if (controllers[0])
+                    {
+                        uiGridCtrl = controllers[0];
+                    }
+                    if (controllers[1])
+                    {
+                        renderContainerCtrl = controllers[1];
+                    }
+                    if (controllers[2])
+                    {
+                        ngModel = controllers[2];
+                    }
+                    var grid = uiGridCtrl.grid;
 
-					$elm.on('blur', function (evt)
-					{
-						$scope.$emit(uiGridEditConstants.events.END_CELL_EDIT);
-						return true;
-					});
+                    $elm.on('blur', function (evt)
+                    {
+                        $scope.$emit(uiGridEditConstants.events.END_CELL_EDIT);
+                        return true;
+                    });
 
-					// Just change the boolean on edit start
-					$scope.$on(uiGridEditConstants.events.BEGIN_CELL_EDIT, function (evt, triggerEvent)
-					{
-						//jQuery($elm[0]).focus(); // TODO: reactivate without jquery
+                    // Just change the boolean on edit start
+                    $scope.$on(uiGridEditConstants.events.BEGIN_CELL_EDIT, function (evt, triggerEvent)
+                    {
+                        //jQuery($elm[0]).focus(); // TODO: reactivate without jquery
 						/*$timeout(function() {
 							$($elm[0]).click();
 						});*/ // changing the value on "enter edit mode" event is actually really disturbing for the user. Disabled for now.
-					});
-				}
-			};
-		}
-	};
+                    });
+                }
+            };
+        }
+    };
 }]);
 
 
