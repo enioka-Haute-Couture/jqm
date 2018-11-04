4fa0f7f6a1a915cf7b591b8794e72af4ec37a31b
Marc-Antoine Gouillart <marsu_pilami@msn.com>
2018-11-04 15:58:30 +0100

    Fixed session issue on ws path

--- jqm-all/jqm-ws/src/main/java/com/enioka/jqm/webui/shiro/SessionEvaluator.java
+++ jqm-all/jqm-ws/src/main/java/com/enioka/jqm/webui/shiro/SessionEvaluator.java
@@ -1,17 +1,35 @@
 package com.enioka.jqm.webui.shiro;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.shiro.mgt.SessionStorageEvaluator;
-import org.apache.shiro.subject.Subject;;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.subject.support.DefaultSubjectContext;
+import org.apache.shiro.web.util.WebUtils;;
 
 public class SessionEvaluator implements SessionStorageEvaluator
 {
     @Override
     public boolean isSessionStorageEnabled(Subject subject)
     {
+        // If disabled in request (e.g. by using the noSessionCreation filter, it stays disabled.
+        if (WebUtils.isWeb(subject))
+        {
+            HttpServletRequest request = WebUtils.getHttpRequest(subject);
+            Object o = request.getAttribute(DefaultSubjectContext.SESSION_CREATION_ENABLED);
+            if (o != null && !((Boolean) o))
+            {
+                return false;
+            }
+        }
+
+        // Then only allow humans, not API-only users, to create a session
         if (subject.hasRole("human"))
         {
             return true;
         }
+
+        // By default, no sessions allowed.
         return false;
     }
 }
