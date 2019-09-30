969f023266978a630d24d4216c6efede2d2880e3
Cyril Rossi <cyril.rossi@enioka.com>
2019-09-30 17:44:53 +0200

    #339 : refactor, use try-with-resources statement.

--- jqm-all/jqm-model/src/main/java/com/enioka/jqm/jdbc/ScriptRunner.java
+++ jqm-all/jqm-model/src/main/java/com/enioka/jqm/jdbc/ScriptRunner.java
@@ -16,58 +16,48 @@ public class ScriptRunner
 
     public static void run(DbConn cnx, String classpath)
     {
-        InputStream is = null;
-
-        try
+        try (InputStream is = ScriptRunner.class.getResourceAsStream(classpath))
         {
-            is = ScriptRunner.class.getResourceAsStream(classpath);
+            if (is == null)
+            {
+                throw new RuntimeException("cannot find db script " + classpath);
+            }
+
+            String line;
+            StringBuilder sb = new StringBuilder();
+            try (InputStreamReader isr = new InputStreamReader(is, "UTF-8"))
+            {
+                BufferedReader br = new BufferedReader(isr);
+
+                line = br.readLine();
+                while (line != null)
+                {
+                    sb.append(line.replace("~", ";"));
+                    sb.append(nl);
+
+                    if (line.contains(";"))
+                    {
+                        // End of order - run it.
+                        cnx.runRawUpdate(sb.substring(0, sb.length() - 1 - nl.length()));
+                        sb = new StringBuilder();
+                    }
+
+                    line = br.readLine();
+                }
+
+            }
+            catch (UnsupportedEncodingException e)
+            {
+                throw new RuntimeException("SQL script encoding issue", e);
+            }
+            catch (IOException e)
+            {
+                throw new RuntimeException("SQL script cannot be read", e);
+            }
         }
         catch (Exception e)
         {
             throw new RuntimeException("cannot find db script " + classpath, e);
-        }
-        if (is == null)
-        {
-            throw new RuntimeException("cannot find db script " + classpath);
-        }
-
-        InputStreamReader isr = null;
-        String line;
-        StringBuilder sb = new StringBuilder();
-        try
-        {
-            isr = new InputStreamReader(is, "UTF-8");
-            BufferedReader br = new BufferedReader(isr);
-
-            line = br.readLine();
-            while (line != null)
-            {
-                sb.append(line.replace("~", ";"));
-                sb.append(nl);
-
-                if (line.contains(";"))
-                {
-                    // End of order - run it.
-                    cnx.runRawUpdate(sb.substring(0, sb.length() - 1 - nl.length()));
-                    sb = new StringBuilder();
-                }
-
-                line = br.readLine();
-            }
-
-        }
-        catch (UnsupportedEncodingException e)
-        {
-            throw new RuntimeException("SQL script encoding issue", e);
-        }
-        catch (IOException e)
-        {
-            throw new RuntimeException("SQL script cannot be read", e);
-        }
-        finally
-        {
-            DbHelper.closeQuietly(isr);
-            DbHelper.closeQuietly(is);
         }
 
         jqmlogger.trace("File {} was run OK", classpath);
