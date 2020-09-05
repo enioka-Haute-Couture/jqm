55e82d94995320e70b8652bf41aa49d9d35980a1
Marc-Antoine Gouillart <marsu_pilami@msn.com>
2020-09-05 10:41:43 +0200

    Fixed #402 - no deliverable displayed in UI

--- jqm-all/jqm-ws/src/main/angular/components/history/history.detail.component.js
+++ jqm-all/jqm-ws/src/main/angular/components/history/history.detail.component.js
@@ -2,9 +2,18 @@
 
 import template from './history.detail.template.html';
 
-//  function ($scope, $http, $uibModal, ji) {
 class HistoryDetailController
 {
+    $doCheck()
+    {
+        if(this.ji !== this.previousJi || this.show !== this.previousShow)
+        {
+            this.dels.length = 0;
+            this.getdel();
+            this.previousJi = this.ji;
+            this.previousShow = this.show;
+        }
+    }
 
     // From bindings : ji, show.
     constructor($http)
@@ -12,6 +21,11 @@ class HistoryDetailController
         this.$http = $http;
         this.dialogId = ("dlg-" + Math.random()).replace('.', '');
         this.dels = [];
+        this.previousJi = null;
+        this.previousShow = null;
+
+        this.getdel = this.getdel.bind(this);
+        this.getdelOk = this.getdelOk.bind(this);
 
         this.getdel();
     }
@@ -20,8 +34,8 @@ class HistoryDetailController
     {
         if (this.show && this.ji)
         {
-            this.$http.get("ws/client/ji/" + this.ji.id + "/files").then(this.getdelOk.bind(this));
-        }
+            this.$http.get("ws/client/ji/" + this.ji.id + "/files").then(this.getdelOk);
+        }        
     };
 
     getdelOk(response)
