ac8fd9b77a8879e13ca5038f5c8e1d33c10cf3d9
Marc-Antoine Gouillart <marsu_pilami@msn.com>
2020-09-05 11:24:34 +0200

    Fixed #400: Oracle CURRENT_TIMESTAMP has a TZ.

--- jqm-all/jqm-model/src/main/java/com/enioka/jqm/jdbc/DbImplOracle.java
+++ jqm-all/jqm-model/src/main/java/com/enioka/jqm/jdbc/DbImplOracle.java
@@ -39,7 +39,8 @@ public class DbImplOracle extends DbAdapter
                 .replace("UNIX_MILLIS()", "JQM_PK.currval").replace("IN(UNNEST(?))", "IN(?)")
                 .replace("CURRENT_TIMESTAMP - 1 MINUTE", "(CURRENT_TIMESTAMP - 1/1440)")
                 .replace("CURRENT_TIMESTAMP - ? SECOND", "(CURRENT_TIMESTAMP - ?/86400)").replace("FROM (VALUES(0))", "FROM DUAL")
-                .replace("BOOLEAN", "NUMBER(1)").replace("true", "1").replace("false", "0").replace("__T__", this.tablePrefix);
+                .replace("BOOLEAN", "NUMBER(1)").replace("true", "1").replace("false", "0").replace("__T__", this.tablePrefix)
+                .replace("CURRENT_TIMESTAMP", "SYS_EXTRACT_UTC(CURRENT_TIMESTAMP)");
     }
 
     @Override
