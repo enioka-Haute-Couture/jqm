f769ec77fb57e9c7e654a9d934c241c317408c27
Hugo Wahl <hugo.wahl@epita.fr>
2020-10-20 15:25:28 +0200

    Modified the Activator to use the loader as a DbAdapter loader

--- jqm-all/jqm-loader/src/main/java/com/enioka/jqm/loader/Activator.java
+++ jqm-all/jqm-loader/src/main/java/com/enioka/jqm/loader/Activator.java
@@ -1,5 +1,7 @@
 package com.enioka.jqm.loader;
 
+import com.enioka.jqm.jdbc.DbAdapter;
+
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.annotation.bundle.Header;
@@ -7,8 +9,18 @@ import org.osgi.annotation.bundle.Header;
 @Header(name = org.osgi.framework.Constants.BUNDLE_ACTIVATOR, value = "${@class}")
 public class Activator implements BundleActivator
 {
+    public Loader<DbAdapter> loader;
+
     public void start(BundleContext context) throws Exception
     {
+        loader = new Loader<DbAdapter>(context, DbAdapter.class, null);
+        loader.start();
+        DbAdapter adapter = loader.getService();
+
+        if (adapter != null)
+        {
+            System.out.println("Found");
+        }
     }
 
     public void stop(BundleContext context) throws Exception
