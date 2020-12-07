d920b982db5bcffed65f71d639adb69e931b339b
Hugo Wahl <hugo.wahl@epita.fr>
2020-12-07 15:13:17 +0100

    Added the annotation in the Activator of jndi-context

--- jqm-all/jqm-jndi-context/src/main/java/com/enioka/jqm/jndi/JndiBundleActivator.java
+++ jqm-all/jqm-jndi-context/src/main/java/com/enioka/jqm/jndi/JndiBundleActivator.java
@@ -2,11 +2,13 @@ package com.enioka.jqm.jndi;
 
 import javax.naming.spi.NamingManager;
 
+import org.osgi.annotation.bundle.Header;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@Header(name = org.osgi.framework.Constants.BUNDLE_ACTIVATOR, value = "${@class}")
 public class JndiBundleActivator implements BundleActivator
 {
     private static Logger jqmlogger = LoggerFactory.getLogger(JndiBundleActivator.class);
