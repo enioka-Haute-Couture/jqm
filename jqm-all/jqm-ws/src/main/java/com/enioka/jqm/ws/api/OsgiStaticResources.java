c1ce5dbec0ad983d534c57fbc2d23b31c217b520
n.dawartz <niels@dawartz.dk>
2021-10-14 14:59:18 +0200

    add path and prefix of static resources

--- jqm-all/jqm-ws/src/main/java/com/enioka/jqm/ws/api/OsgiStaticResources.java
+++ jqm-all/jqm-ws/src/main/java/com/enioka/jqm/ws/api/OsgiStaticResources.java
@@ -6,10 +6,10 @@ import org.osgi.service.http.whiteboard.propertytypes.HttpWhiteboardResource;
 /**
  * Configuration for serving static files through the OSGi HTTP whiteboard. Simply serves our static files (resulting from the JS build).
  */
-@Component(service = OsgiStaticResourceServer.class, property = {
-        org.osgi.framework.Constants.SERVICE_RANKING + ":Integer=" + Integer.MIN_VALUE,
+@Component(service = OsgiStaticResources.class, property = { org.osgi.framework.Constants.SERVICE_RANKING + ":Integer=" + Integer.MIN_VALUE,
         "osgi.http.whiteboard.context.select=(osgi.http.whiteboard.context.name=MAIN_HTTP_CTX)" })
-@HttpWhiteboardResource(pattern = "/", prefix = "dist/index.html")
-public class OsgiStaticResourceServer
+@HttpWhiteboardResource(pattern = "/static/*", prefix = "/dist/static")
+public class OsgiStaticResources
 {
+
 }
