986e4488a801cc7a4421757e8ec9531ccb6d55be
Paulin de Naurois <paulin.de.naurois@enioka.com>
2022-02-25 16:42:21 +0100

    React-v3 : Fix job definition API

--- jqm-all/jqm-ws/src/main/react/src/components/JobDefinitions/JobDefinitionsAPI.tsx
+++ jqm-all/jqm-ws/src/main/react/src/components/JobDefinitions/JobDefinitionsAPI.tsx
@@ -3,7 +3,7 @@ import APIService from "../../utils/APIService";
 import { useNotificationService } from "../../utils/NotificationService";
 import { JobDefinition, JobType } from "./JobDefinition";
 
-function pathTypeToApplicationType(pathType: string): JobType {
+function pathTypeToJobType(pathType: string): JobType {
     switch (pathType) {
         case "DIRECTEXECUTABLE":
             return JobType.process;
@@ -13,6 +13,26 @@ function pathTypeToApplicationType(pathType: string): JobType {
         default:
             return JobType.java;
     }
+}
+
+function jobDefinitionToPayload(jobDefinition: JobDefinition): any {
+    let payload: any = jobDefinition;
+
+    payload = {
+        ...payload,
+        application: jobDefinition.tags.application,
+        module: jobDefinition.tags.module,
+        keyword1: jobDefinition.tags.keyword1,
+        keyword2: jobDefinition.tags.keyword2,
+        keyword3: jobDefinition.tags.keyword3,
+        javaClassName: jobDefinition.properties.javaClassName ? jobDefinition.properties.javaClassName : "com.company.product.ClassName",
+        jarPath: jobDefinition.properties.jarPath,
+        pathType: jobDefinition.properties.pathType,
+    }
+    delete payload.tags;
+    delete payload.properties;
+
+    return payload;
 }
 
 const API_URL = "/admin/jd";
@@ -36,11 +56,11 @@ export const useJobDefinitionsAPI = () => {
                         keyword2: jobDefinition.keyword2,
                         keyword3: jobDefinition.keyword3,
                     },
-                    jobType: pathTypeToApplicationType(jobDefinition.pathType),
+                    jobType: pathTypeToJobType(jobDefinition.pathType),
                     properties: {
                         javaClassName: jobDefinition.javaClassName,
                         jarPath: jobDefinition.jarPath,
-                        jobType: pathTypeToApplicationType(
+                        jobType: pathTypeToJobType(
                             jobDefinition.pathType
                         ),
                         pathType: jobDefinition.pathType,
@@ -52,7 +72,7 @@ export const useJobDefinitionsAPI = () => {
 
     const createJobDefinition = useCallback(
         async (newJobDefinition: JobDefinition) => {
-            return APIService.post(API_URL, newJobDefinition)
+            return APIService.post(API_URL, jobDefinitionToPayload(newJobDefinition))
                 .then(() => {
                     fetchJobDefinitions();
                     displaySuccess(
@@ -83,7 +103,7 @@ export const useJobDefinitionsAPI = () => {
 
     const updateJobDefinition = useCallback(
         async (jobDefinition: JobDefinition) => {
-            return APIService.put(`${API_URL}/${jobDefinition.id}`, jobDefinition)
+            return APIService.put(`${API_URL}/${jobDefinition.id}`, jobDefinitionToPayload(jobDefinition))
                 .then(() => {
                     fetchJobDefinitions();
                     displaySuccess("Successfully saved job definition");
