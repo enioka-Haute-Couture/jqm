b71400ae08e86d97dd1d5e802dd3a9723e2003eb
Paulin de Naurois <pna@enioka.com>
2025-02-21 15:45:02 +0100

    Feat: save runs table pagination state in a context
    Closes #341

--- jqm-all/jqm-ws/src/main/react/src/App.tsx
+++ jqm-all/jqm-ws/src/main/react/src/App.tsx
@@ -22,6 +22,7 @@ import RunsPage from "./components/Runs/RunsPage";
 import { JobDefinitionsPage } from "./components/JobDefinitions/JobDefinitionsPage";
 import { ForceLogin } from "./utils/ForceLogin";
 import { AuthProvider } from "./utils/AuthService";
+import { RunsPaginationProvider } from "./utils/RunsPaginationProvider";
 
 
 
@@ -55,42 +56,44 @@ function App() {
                                 }}
                             >
                                 <MenuWrapper>
-                                    <Switch>
-                                        <Route path="/nodes" exact={true}>
-                                            <NodesPage />
-                                        </Route>
-                                        <Route path="/queues" exact={true}>
-                                            <QueuesPage />
-                                        </Route>
-                                        <Route
-                                            path="/clusterwide-parameters"
-                                            exact={true}
-                                        >
-                                            <ClusterwideParametersPage />
-                                        </Route>
-                                        <Route path="/job-definitions" exact={true}>
-                                            <JobDefinitionsPage />
-                                        </Route>
-                                        <Route path="/users" exact={true}>
-                                            <UsersPage />
-                                        </Route>
-                                        <Route path="/mappings" exact={true}>
-                                            <MappingsPage />
-                                        </Route>
-                                        <Route path="/roles" exact={true}>
-                                            <RolesPage />
-                                        </Route>
-                                        <Route path="/jndi" exact={true}>
-                                            <JndiPage />
-                                        </Route>
-                                        <Route path="/runs" exact={true}>
-                                            <RunsPage />
-                                        </Route>
-                                        <Route path="/" exact={true}>
-                                            <HomePage />
-                                        </Route>
-                                        <Redirect to="/" />
-                                    </Switch>
+                                    <RunsPaginationProvider>
+                                        <Switch>
+                                            <Route path="/nodes" exact={true}>
+                                                <NodesPage />
+                                            </Route>
+                                            <Route path="/queues" exact={true}>
+                                                <QueuesPage />
+                                            </Route>
+                                            <Route
+                                                path="/clusterwide-parameters"
+                                                exact={true}
+                                            >
+                                                <ClusterwideParametersPage />
+                                            </Route>
+                                            <Route path="/job-definitions" exact={true}>
+                                                <JobDefinitionsPage />
+                                            </Route>
+                                            <Route path="/users" exact={true}>
+                                                <UsersPage />
+                                            </Route>
+                                            <Route path="/mappings" exact={true}>
+                                                <MappingsPage />
+                                            </Route>
+                                            <Route path="/roles" exact={true}>
+                                                <RolesPage />
+                                            </Route>
+                                            <Route path="/jndi" exact={true}>
+                                                <JndiPage />
+                                            </Route>
+                                            <Route path="/runs" exact={true}>
+                                                <RunsPage />
+                                            </Route>
+                                            <Route path="/" exact={true}>
+                                                <HomePage />
+                                            </Route>
+                                            <Redirect to="/" />
+                                        </Switch>
+                                    </RunsPaginationProvider>
                                 </MenuWrapper>
                             </SnackbarProvider>
                         </ThemeProvider>
