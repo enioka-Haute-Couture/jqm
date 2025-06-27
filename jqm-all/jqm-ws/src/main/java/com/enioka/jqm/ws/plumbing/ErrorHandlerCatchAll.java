04bf835c261466f27d06dd605bb6fc2bb971bc59
Marc-Antoine Gouillart <marsu_pilami@msn.com>
2025-06-27 14:55:26 +0200

    #601 - Added new WS error handler (#602)

---
+++ jqm-all/jqm-ws/src/main/java/com/enioka/jqm/ws/plumbing/ErrorHandlerCatchAll.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright © 2013 enioka. All rights reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.enioka.jqm.ws.plumbing;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.enioka.jqm.ws.api.ErrorDto;
+
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.HttpHeaders;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class ErrorHandlerCatchAll implements ExceptionMapper<Exception>
+{
+    private static final Logger log = LoggerFactory.getLogger("RestClientException");
+
+    @Context
+    private HttpHeaders headers;
+
+    @Override
+    public Response toResponse(Exception e)
+    {
+        MediaType type = headers.getMediaType();
+        if (type != MediaType.APPLICATION_JSON_TYPE && type != MediaType.APPLICATION_XML_TYPE)
+        {
+            type = MediaType.APPLICATION_JSON_TYPE;
+        }
+        log.error("An unexpected error occurred while processing a REST call", e);
+        var dto = new ErrorDto("An unexpected error occurred. Please contact your administrator with the error details.", 999, e,
+                Status.INTERNAL_SERVER_ERROR);
+        return Response.status(Status.INTERNAL_SERVER_ERROR).entity(dto).type(type).build();
+    }
+}
