91221d65bef24a779294247e42f5c6050e48f85c
Joachim Le Bray <joachim.lebray@enioka.com>
2025-07-11 16:57:54 +0200

    fix test file

--- jqm-all/jqm-api-client/jqm-api-client-jdbc/src/test/java/com/enioka/jqm/client/jdbc/api/test/BasicTest.java
+++ jqm-all/jqm-api-client/jqm-api-client-jdbc/src/test/java/com/enioka/jqm/client/jdbc/api/test/BasicTest.java
@@ -228,9 +228,9 @@ public class BasicTest
             long jobDefdId = JobDef.create(cnx, "test description", "class", null, "jar", qId, 1, "appName", null, null, null, null, null,
                     false, null, PathType.FS);
 
-            JobInstance.enqueue(cnx, com.enioka.jqm.model.State.RUNNING, qId, jobDefdId, null, null, null, null, null, null, null, null,
+            JobInstance.enqueue(cnx, com.enioka.jqm.model.State.RUNNING, qId, jobDefdId, null, null, null, null, null, null, null, null, null,
                     null, false, false, null, 1, Instruction.RUN, new HashMap<>());
-            JobInstance.enqueue(cnx, com.enioka.jqm.model.State.RUNNING, qId, jobDefdId, null, null, null, null, null, null, null, null,
+            JobInstance.enqueue(cnx, com.enioka.jqm.model.State.RUNNING, qId, jobDefdId, null, null, null, null, null, null, null, null, null,
                     null, false, false, null, 1, Instruction.RUN, new HashMap<>());
             cnx.commit();
 
