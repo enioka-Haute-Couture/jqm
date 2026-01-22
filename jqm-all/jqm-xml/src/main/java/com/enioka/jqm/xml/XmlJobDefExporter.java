1725f48b35aedf03067135d869598f0f482f215c
Paulin de Naurois <pna@enioka.com>
2026-01-22 10:24:43 +0100

    Fix critical and high CVE vulnerabilities (#667)

--- jqm-all/jqm-xml/src/main/java/com/enioka/jqm/xml/XmlJobDefExporter.java
+++ jqm-all/jqm-xml/src/main/java/com/enioka/jqm/xml/XmlJobDefExporter.java
@@ -26,10 +26,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.output.Format;
-import org.jdom.output.XMLOutputter;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
 
 import com.enioka.jqm.jdbc.DbConn;
 import com.enioka.jqm.model.Cl;
